0ee457ec3346db2b7d7664d9698589791cc82675
Robert Hafner <tedivm@tedivm.com>
2008-11-23 06:55:32 +0000

    Corrected error that was causing packages not to load correctly (specifically, only the default class and error pages would load).
    A new class, RuntimeConfig, was created to move certain variables away from the Config class. Various other classes were updated to use it, such as the Engine class.

---
+++ system/classes/RuntimeConfig.class.php
@@ -0,0 +1,59 @@
+<?php
+class RuntimeConfig implements ArrayAccess 
+{
+	protected $config = array();
+	static $instance;
+		
+	private function __construct()
+	{
+		$get = Get::getInstance();
+		
+		$this->config['engine'] = ((isset($get['engine'])) ? $get['engine'] : 'Html');
+		$this->config['action'] = (isset($get['action'])) ? $get['action'] : 'Default';
+		$this->config['package'] = $get['package'];
+		$this->config['moduleId'] = $get['moduleId'];
+		$this->config['siteId'] = $get['siteId'];
+		$this->config['id'] = $get['id'];
+		
+	}
+
+	public static function getInstance()
+	{
+		if(!isset(self::$instance)){
+			$object = __CLASS__;
+			self::$instance = new $object;			
+		}
+		return self::$instance;
+	}	
+
+	public function offsetExists($offset)
+	{
+		if(isset($this->config[$offset]))
+		{
+			return true;
+		}
+			
+		return false;
+		
+	}
+	
+	public function offsetGet($offset)
+	{
+		return $this->config[$offset];
+	}
+	
+	
+	public function offsetSet($key, $value)
+	{
+		$this->config[$key] = $value;
+	}
+	
+	public function offsetUnset($offset)
+	{
+		unset($this->config[$offset]);
+		return true;
+	}
+	
+}
+
+?>
