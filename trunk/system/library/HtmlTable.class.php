3c861de0ac27d7346771b3184dde128c9720e25c
rhafner <rhafner@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2008-12-09 21:35:32 +0000

    HtmlTable- 	Added CSS library support- class now tells the theme class to load "table.TableName.css".

--- trunk/system/library/HtmlTable.class.php
+++ trunk/system/library/HtmlTable.class.php
@@ -9,21 +9,21 @@ class HtmlTable implements ArrayAccess
 	protected $columns;
 	protected $classes = array();
 	protected $properties = array();
-	
-	
+
+
 	public function __construct($name, $columns)
 	{
 		if(!is_string($name))
 			throw new BentoError('Unable to create table without a name.');
 
-		
+
 		if(!is_array($columns))
 			throw new BentoError('Unable to create table without columns.');
-			
+
 		$this->columns = $columns;
 		$this->name = $name;
 	}
-	
+
 	public function addClass($class)
 	{
 		if(is_array($class))
@@ -33,36 +33,44 @@ class HtmlTable implements ArrayAccess
 			$this->classes[] = $class;
 		}
 	}
-	
+
 	public function makeDisplay()
 	{
 		$div = new HtmlObject('div');
 		$table = $div->insertNewHtmlObject('table');
 		$table->property('id', $this->name);
-		
+
 		$table->addClass($this->classes);
 		$table->property($this->properties);
-		
-		foreach($this->properties as $property => $value)
-		{
-			$table;
-		}
-		
+
 		$tableBody = $table->insertNewHtmlObject('tbody');
-		
+
 		if($this->displayHeader)
 			$this->addHeader($tableBody);
 
 		$this->addData($tableBody);
-		
+
+
+		if(class_exists('ActivePage', false))
+		{
+			$page = ActivePage::getInstance();
+			$page->addStartupScript($javascript);
+			$page->addJQueryInclude($jqueryPlugins);
+			$page->addCss($this->name, 'tables');
+			//$page->
+
+		}else{
+
+		}
+
 		return (string) $div;
 	}
-	
+
 	public function nextRow()
 	{
 		$this->data[] = array();
 	}
-	
+
 	protected function addHeader($tableBody)
 	{
 		if($this->singleHeader)
@@ -71,12 +79,12 @@ class HtmlTable implements ArrayAccess
 			$th = $tableBody->insertHtmlObject('tr')->insertHtmlObject('th');
 			$th->addClass('full');
 			$th->property('colspan', $columns);
-			
+
 		}else{
-			
+
 		}
 	}
-	
+
 	protected function addData($tableBody)
 	{
 		$x = 1;
@@ -84,20 +92,20 @@ class HtmlTable implements ArrayAccess
 		{
 			if(count($row) < 1)
 				break;
-			
+
 			$tableRow = $tableBody->insertNewHtmlObject('tr');
-			
-			
+
+
 			if(isset($row['properties']))
 				$tableRow->property($row['properties']);
-				
+
 			if(isset($row['classes']))
-				$tableRow->addClass($row['classes']);				
-				
+				$tableRow->addClass($row['classes']);
+
 			$x++;
 			if($x % 2)
 				$tableRow->addClass('bg');
-				
+
 			$rowTemp = array();
 			foreach($this->columns as $index => $name)
 			{
@@ -108,50 +116,50 @@ class HtmlTable implements ArrayAccess
 					$rowTemp[$index] = $this->emptyCell;
 				}
 			}
-			
+
 			$first = true;
 			foreach($rowTemp as $rowContent)
 			{
 				$td = $tableRow->insertNewHtmlObject('td')->
 					wrapAround($rowContent);
-					
-				if($first)	
+
+				if($first)
 				{
 					$td->addClass('first');
 					$first = false;
 				}
 			}
 			$td->addClass('last');
-			
+
 		}
-		
+
 	}
-	
-	
+
+
 	public function offsetSet($offset, $value)
 	{
 		$keys = array_keys($this->data);
-		$this->data[array_pop($keys)][$offset] = $value; 
+		$this->data[array_pop($keys)][$offset] = $value;
 	}
-	
+
 	public function offsetGet($offset)
 	{
 		$keys = array_keys($this->data);
 		return $this->data[array_pop($keys)][$offset];
 	}
-	
+
 	public function offsetExists($offset)
 	{
 		$keys = array_keys($this->data);
 		return isset($this->data[array_pop($keys)][$offset]);
 	}
-	
+
 	public function offsetUnset($offset)
 	{
 		$keys = array_keys($this->data);
 		unset($this->data[array_pop($keys)][$offset]);
-	}	
-	
+	}
+
 }
 
 
