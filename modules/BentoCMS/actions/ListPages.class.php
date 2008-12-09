2d8a7d865067c38c580a1f37d06c78a47722bad7
Robert Hafner <tedivm@tedivm.com>
2008-12-09 07:19:17 +0000

    CMS- 	Added 'status' attribute. Current statuses include 'active', 'draft' and 'deleted'. 	Default class (which handles page outputting) now alters the display based on status. It also checks permissions to make sure the person trying to view it is allowed to.

--- modules/BentoCMS/actions/ListPages.class.php
+++ modules/BentoCMS/actions/ListPages.class.php
@@ -3,21 +3,21 @@
 class BentoCMSActionListPages extends PackageAction
 {
 	static $requiredPermission = 'Read';
-	
+
 	public $AdminSettings = array('linkLabel' => 'List Page',
 									'linkTab' => 'Content',
 									'headerTitle' => 'List Pages',
-									'linkContainer' => 'CMS');	
+									'linkContainer' => 'CMS');
 	protected $pages;
-	
+
 	public function logic()
 	{
 		$packageList = new PackageInfo('BentoCMS');
-		
+
 		$modules = $packageList->getModules('Read');
 		$db = dbConnect('default_read_only');
-		
-		$pages = array();
+
+		$list = array();
 		foreach($modules as $module)
 		{
 			$pageList = new BentoCMSPageList($module['modId']);
@@ -27,7 +27,7 @@ class BentoCMSActionListPages extends PackageAction
 
 		$this->pages = $list;
 	}
-	
+
 	public function viewAdmin()
 	{
 		foreach($this->pages as $locationString => $pageList)
@@ -35,50 +35,50 @@ class BentoCMSActionListPages extends PackageAction
 			$table = new HtmlTable('Table', array('name', 'editLink', 'deleteLink', 'pageLink'));
 			$table->addClass(array('listing'));
 			$x = 1;
-			
-			
+
+
 			foreach($pageList as $pageName => $page)
 			{
-				
+
 				$table['name'] = $pageName;
-				
+
 				$url = new Url();
 				$url->property('module', $page->property('module'));
 				$url->property('engine', 'Admin');
 
 				$url->property('id', $page->property('id'));
-				
-				
+
+
 				$url->property('action', 'EditPage');
 				$editLink = new HtmlObject('a');
 				$editLink->property('href', (string) $url);
 				$editLink->wrapAround('Edit');
-								
+
 				$table['editLink'] = (string) $editLink;
-				
-				
+
+
 				$url->property('action', 'RemovePage');
 				$deleteLink = new HtmlObject('a');
 				$deleteLink->property('href', (string) $url);
 				$deleteLink->wrapAround('Delete');
-				
+
 				$table['deleteLink'] = (string) $deleteLink;
-				
-				
-				
-				
+
+
+
+
 				$table['pageLink'] = 'blah blah blah';
 				$table->nextRow();
 			}
 		}
-		
+
 		return $table->makeDisplay();
 	}
-	
-	
-	
-	
-	
+
+
+
+
+
 }
 
 
