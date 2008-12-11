e7fbea310d45bcb2a69d9ec1bbefcd2ec60e07f5
rhafner <rhafner@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2008-12-11 09:03:26 +0000

    whitespace cleanup.

--- trunk/modules/BentoBase/actions/CacheSettings.class.php
+++ trunk/modules/BentoBase/actions/CacheSettings.class.php
@@ -1,52 +1,52 @@
 <?php
 
-class BentoBaseActionCacheSettings extends PackageAction  
+class BentoBaseActionCacheSettings extends PackageAction
 {
 	static $requiredPermission = 'System';
-	
+
 	public $AdminSettings = array('linkLabel' => 'Settings',
 									'linkTab' => 'System',
 									'headerTitle' => 'Cache Settings',
 									'linkContainer' => 'Cache');
-									
+
 	protected $form;
 	protected $success = false;
-					
+
 	public function logic()
 	{
 		$info = InfoRegistry::getInstance();
 		$configIni = new IniFile($info->Configuration['path']['config'] . 'configuration.php');
-		
+
 		$this->form = new Form('cacheSettingsForm');
-		
+
 		$cacheHandlers = Cache::getHandlers();
-		
+
 		$handlerInput = $this->form->createInput('cacheHandler');
-		
+
 		$handlerInput->setType('select')->
 				setLabel('Cache Method');
-		
+
 		foreach($cacheHandlers as $handlerName => $handlerClass)
 		{
 			$attributes = array();
 			if($configIni->get('cache', 'handler') == $handlerName)
 				$attributes = array('selected' => 'selected');
-			$handlerInput->setOptions($handlerName, $handlerName, $attributes);	
+			$handlerInput->setOptions($handlerName, $handlerName, $attributes);
 		}
-		
+
 		if($this->form->checkSubmit())
 		{
 			$inputHandler = $this->form->getInputhandler();
-			
+
 			$configIni->set('cache', 'handler', $inputHandler['cacheHandler']);
 			$this->success = $configIni->write();
-			
-			
-			
-			
+
+
+
+
 		}
 	}
-	
+
 	public function viewAdmin()
 	{
 		if($this->form->wasSubmitted())
@@ -57,10 +57,10 @@ class BentoBaseActionCacheSettings extends PackageAction
 			}
 		}
 
-		$output .= $this->form->makeDisplay();		
+		$output .= $this->form->makeDisplay();
 		return $output;
 	}
-									
+
 }
 
 
