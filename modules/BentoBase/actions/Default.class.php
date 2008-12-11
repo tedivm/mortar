0fbb3a79c2f095a7bb16ea0b01ff24daca4a27e0
Robert Hafner <tedivm@tedivm.com>
2008-12-11 09:03:26 +0000

    whitespace cleanup.

--- modules/BentoBase/actions/Default.class.php
+++ modules/BentoBase/actions/Default.class.php
@@ -1,37 +1,37 @@
 <?php
 
-class BentoBaseActionDefault extends PackageAction  
+class BentoBaseActionDefault extends PackageAction
 {
 	//static $requiredPermission = 'Read';
-	
+
 	protected function logic()
 	{
-		
+
 	}
-	
+
 	public function viewHtml()
 	{
 		$config = Config::getInstance();
-		
+
 		if($config['id'] == 'admin')
 		{
 			$adminUrl = new Url();
 			$adminUrl->engine = 'Admin';
-			
+
 			//return $adminUrl;
-			
+
 			header('Location: ' . $adminUrl);
-		}		
-		
+		}
+
 		$page = ActivePage::getInstance();
 		$page['title'] = 'Test Title';
 		return 'This is the main page';
 	}
-	
+
 	public function viewAdmin()
 	{
 		$user = ActiveUser::getInstance();
-		
+
 		if($user->getName() == 'guest')
 		{
 			$url = $this->linkToSelf();
@@ -39,18 +39,18 @@ class BentoBaseActionDefault extends PackageAction
 			$url->property('engine', 'Admin');
 			header('Location:' . $url);
  		}
-			
-		
-		
-		
+
+
+
+
 		$get = Get::getInstance();
 		$tab = ($get['tab']) ? $get['tab'] : 'Main';
 		$this->AdminSettings['linkTab'] = $tab;
 		$this->AdminSettings['headerTitle'] = $tab;
 		return 'This is the main page';
 	}
-	
-	
+
+
 }
 
 
