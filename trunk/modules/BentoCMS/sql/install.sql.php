4d58f5bf9f645520cbea32158c60778278af59df
rhafner <rhafner@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2008-12-12 10:33:36 +0000

    Cms- 	Changed table names to begin with with module name.

--- trunk/modules/BentoCMS/sql/install.sql.php
+++ trunk/modules/BentoCMS/sql/install.sql.php
@@ -1,7 +1,7 @@
 /******************** Add Table: cmsContent ************************/
 
 /* Build Table Structure */
-CREATE TABLE cmsContent
+CREATE TABLE BentoCMS_Content
 (
 	location_id BIGINT UNSIGNED NOT NULL,
 	contentVersion INTEGER UNSIGNED NOT NULL,
@@ -13,13 +13,13 @@ CREATE TABLE cmsContent
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 /* Table Items: cmsContent */
-ALTER TABLE cmsContent ADD CONSTRAINT pkcmsContent
+ALTER TABLE BentoCMS_Content ADD CONSTRAINT pkBentoCMS_Content
 	PRIMARY KEY (location_id, contentVersion);
 
 /******************** Add Table: cmsPages ************************/
 
 /* Build Table Structure */
-CREATE TABLE cmsPages
+CREATE TABLE BentoCMS_Pages
 (
 	location_id BIGINT UNSIGNED NOT NULL,
 	pageCurrentVersion INTEGER UNSIGNED NULL,
@@ -29,18 +29,18 @@ CREATE TABLE cmsPages
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 /* Table Items: cmsPages */
-ALTER TABLE cmsPages ADD CONSTRAINT pkcmsPages
+ALTER TABLE BentoCMS_Pages ADD CONSTRAINT pkBentoCMS_Pages
 	PRIMARY KEY (location_id);
 
 
 /************ Foreign Key: fk_cmsContent_locations ***************/
-ALTER TABLE cmsContent ADD CONSTRAINT fk_cmsContent_locations
+ALTER TABLE BentoCMS_Content ADD CONSTRAINT fk_BentoCMS_Content_locations
 	FOREIGN KEY (location_id) REFERENCES locations (location_id) ON UPDATE NO ACTION ON DELETE NO ACTION
 
 /************ Foreign Key: fk_cmsContent_users ***************/
-ALTER TABLE cmsContent ADD CONSTRAINT fk_cmsContent_users
+ALTER TABLE BentoCMS_Content ADD CONSTRAINT fk_BentoCMS_Content_users
 	FOREIGN KEY (contentAuthor) REFERENCES users (user_id) ON UPDATE NO ACTION ON DELETE NO ACTION
 
 /************ Foreign Key: fk_cmsPages_locations ***************/
-ALTER TABLE cmsPages ADD CONSTRAINT fk_cmsPages_locations
+ALTER TABLE BentoCMS_Pages ADD CONSTRAINT fk_BentoCMS_Pages_locations
 	FOREIGN KEY (location_id) REFERENCES locations (location_id) ON UPDATE NO ACTION ON DELETE NO ACTION
