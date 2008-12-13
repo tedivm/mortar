7e7a54f7c91fcc0f26ffcfbc00d317345a7542de
rhafner <rhafner@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2008-12-13 06:54:27 +0000

    Changed name.

--- trunk/modules/BentoBotch/actions/Technical.class.php
+++ trunk/modules/BentoBotch/actions/Technical.class.php
@@ -1,13 +1,13 @@
 <?php
 
-class BentoBotchActionTechnical extends Action
+class BentoBotchActionTechnicalError extends Action
 {
 	public $AdminSettings = array(	'linkTab' => 'Universal',
-									'headerTitle' => 'Unknown Error');	
-	
+									'headerTitle' => 'Unknown Error');
+
 	public function logic()
 	{
-		
+
 	}
 
 	public function viewHtml()
@@ -15,16 +15,16 @@ class BentoBotchActionTechnical extends Action
 		$output = 'An unknown error has occured.';
 		return $output;
 	}
-	
+
 	public function viewAdmin()
 	{
 		$output = 'An unknown error has occured.';
 		return $output;
 	}
-	
+
 	public function viewJson()
 	{
-		
+
 		return $output;
 	}
 }
