fcfb070b75aa5d7beff472c072e3dec56868e937
Robert Hafner <tedivm@tedivm.com>
2008-12-14 11:48:55 +0000

    BentoBlog 	Added entry display code. 	Enhanced display from the CMS version to include more use information

--- modules/BentoBlog/actions/ViewPost.class.php
+++ modules/BentoBlog/actions/ViewPost.class.php
@@ -1,15 +1,46 @@
 <?php
+AutoLoader::import('BentoCMS');
 
-class BentoBlogActionViewPost extends Action
+class BentoBlogActionViewPost extends BentoCMSActionViewPage
 {
 	static $requiredPermission = 'Read';
-	public function logic()
-	{
+	protected $resourceType = 'Post';
+	protected $resourceClass = 'BentoBlogPost';
 
+
+	protected function htmlContentArea()
+	{
+		$post = new DisplayMaker();
+		$post->set_display_template($this->loadTemplate('ViewPost'));
+		$entry = $this->page;
+
+
+		$user = new User();
+		$user->load_user($entry->property('author'));
+		$post->addContent('author', $user->getName());
+
+		$tagArray = $entry->property('tags');
+		$post->addContent('tags', implode($entry->property('tags'), ', '));
+
+
+	//	$post->addContent('author');
+
+
+		$link = $this->linkToSelf();
+		$link->id = $entry->property('name');
+		$post->addContent('link', $link);
+
+
+		$post->addDate('createdOn', strtotime($entry->property('createdDate')));
+
+
+
+		$revision = $entry->getRevision();
+		$post->addContent('subject', $revision->property('title'));
+
+		$post->addContent('entry', $revision->property('content'));
+
+		return $post->make_display();
 	}
 
-	public function viewAdmin()
-	{
-
-	}
 }
