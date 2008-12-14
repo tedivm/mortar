821e3da23d7f5dc1df0ac8fc1250d64e7c4182ad
Robert Hafner <tedivm@tedivm.com>
2008-12-14 11:41:40 +0000

    Added autoloader import for loading the actions- this only happens every 5 minutes or so, as its cached.

--- system/classes/RuntimeConfig.class.php
+++ system/classes/RuntimeConfig.class.php
@@ -1,50 +1,52 @@
 <?php
-class RuntimeConfig implements ArrayAccess 
+class RuntimeConfig implements ArrayAccess
 {
 	protected $config = array();
 	static $instance;
-		
+
 	private function __construct()
 	{
 		$get = Get::getInstance();
 		$this->config['engine'] = ((isset($get['engine'])) ? $get['engine'] : 'Html');
 		$this->config['action'] = (isset($get['action'])) ? $get['action'] : 'Default';
 		$this->config['moduleId'] = $get['moduleId'];
-		
-		
+
+
 		try{
-			
+
 			if(is_numeric($this->config['moduleId']))
-			{				
+			{
 				$moduleInfo = new ModuleInfo($get['moduleId']);
 				$this->config['package'] = $moduleInfo['Package'];
 				$this->config['packagePath'] = $moduleInfo['PathToPackage'];
-				
+				AutoLoader::import($moduleInfo['Package']);
+
 			}elseif(isset($get['package'])){
 
+				AutoLoader::import($get['package']);
 				$packageInfo = new PackageInfo($get['package']);
 				$this->config['package'] = $packageInfo->getName();
 				$this->config['packagePath'] = $packageInfo->getPath();
-				
+
 			}
-			
+
 		}catch(Exception $e){
-			
+
 		}
-		
+
 		$this->config['siteId'] = $get['siteId'];
 		$this->config['id'] = $get['id'];
-		
+
 	}
 
 	public static function getInstance()
 	{
 		if(!isset(self::$instance)){
 			$object = __CLASS__;
-			self::$instance = new $object;			
+			self::$instance = new $object;
 		}
 		return self::$instance;
-	}	
+	}
 
 	public function offsetExists($offset)
 	{
@@ -52,28 +54,28 @@ class RuntimeConfig implements ArrayAccess
 		{
 			return true;
 		}
-			
+
 		return false;
-		
+
 	}
-	
+
 	public function offsetGet($offset)
 	{
 		return $this->config[$offset];
 	}
-	
-	
+
+
 	public function offsetSet($key, $value)
 	{
 		$this->config[$key] = $value;
 	}
-	
+
 	public function offsetUnset($offset)
 	{
 		unset($this->config[$offset]);
 		return true;
 	}
-	
+
 }
 
 ?>
