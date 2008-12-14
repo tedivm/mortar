7aa1b057415798d3e83dd78511878a7f5fbd5a64
Robert Hafner <tedivm@tedivm.com>
2008-12-14 11:44:18 +0000

    ModuleBase 	Corrected bug which was causes package information not to load in some circumstances.

--- system/abstracts/ModuleBase.abstract.php
+++ system/abstracts/ModuleBase.abstract.php
@@ -4,67 +4,73 @@ abstract class ModuleBase
 {
 	protected $moduleId;
 	protected $moduleName;
-	protected $modulePackage;
+
 	protected $location;
-	protected $settings = array();	
+	protected $settings = array();
+
 	protected $pathToPackage;
 	protected $package;
-	
+
 	public $packageInfo;
-	
+
 	protected function loadSettings()
-	{	
+	{
 		if(isset($this->moduleId))
 		{
 			$info = new ModuleInfo($this->moduleId);
 			$this->location = new Location($info['locationId']);
 			$this->moduleName = $info['Name'];
 			$this->settings = $info->settings;
+			$package = $info['Package'];
+		}elseif(isset($this->package)){
+			$package = $this->package;
 		}
-		
-		if(isset($this->package))
+
+		if(isset($package))
 		{
-			$this->packageInfo = new PackageInfo($this->package);
-			$this->pathToPackage = $this->packageInfo->path;
+			$packageInfo = new PackageInfo($package);
+			$this->packageInfo = $packageInfo;
+			$this->package = $packageInfo->name;
+			$this->pathToPackage = $packageInfo->path;
 		}
 	}
-	
+
 	// temporary until we finish cleanup
 	protected function load_template($name)
 	{
 		return $this->loadTemplate($name);
 	}
-	
+
 	protected function loadTemplate($name)
 	{
 		$config = Config::getInstance();
-		
+
 		$template_path = $config['path']['theme'] . 'packages/' . $this->package . '/' . $name . '.template.php';
-		
+
 		if(file_exists($template_path))
 			return file_get_contents($template_path);
-		
+
 		$template_path = $config['path']['modules'] . $this->package . '/templates/' . $name .'.template.php';
 
 		if(file_exists($template_path))
 			return file_get_contents($template_path);
-				
-	}	
-	
+
+	}
+
 	protected function loadHelper($class)
 	{
 		try {
 			if(!($instance = load_helper($this->package, $class)))
 				throw new BentoNotice('Unable to load helper class ' . $class . ' in package ' . $this->package);
-			
+
 		}catch (Exception $e){
 			return false;
 		}
-		
+
 		return $instance;
 	}
-	
-	
+
+
 }
 
 ?>
