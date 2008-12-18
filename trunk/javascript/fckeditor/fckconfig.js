d9a7d1bd7b3b85ed18ec1a1a4806b9f8fc449ab9
rhafner <rhafner@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2008-12-18 11:18:10 +0000

    Rearranged the 'default' toolbars of the FCKeditor

--- trunk/javascript/fckeditor/fckconfig.js
+++ trunk/javascript/fckeditor/fckconfig.js
@@ -102,17 +102,14 @@ FCKConfig.ToolbarSets["Default"] = [
 	['Source','DocProps','-','Save','NewPage','Preview','-','Templates'],
 	['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
 	['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-	['Form','Checkbox','Radio','TextField','Textarea','Select','Button','ImageButton','HiddenField'],
 	'/',
 	['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
 	['OrderedList','UnorderedList','-','Outdent','Indent','Blockquote','CreateDiv'],
 	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
 	['Link','Unlink','Anchor'],
-	['Image','Flash','Table','Rule','Smiley','SpecialChar','PageBreak'],
+	['FitWindow','ShowBlocks','-','About','Image','Flash','Table','Rule','SpecialChar','PageBreak', 'TextColor','BGColor'],
 	'/',
-	['Style','FontFormat','FontName','FontSize'],
-	['TextColor','BGColor'],
-	['FitWindow','ShowBlocks','-','About']		// No comma for the last row.
+	['Style','FontFormat','FontName','FontSize']	// No comma for the last row.
 ] ;
 
 FCKConfig.ToolbarSets["Basic"] = [
