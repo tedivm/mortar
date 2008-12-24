98066c485810a5b34bd800d2ff9a50476edc6a5f
rhafner <rhafner@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2008-12-24 20:04:35 +0000

    Login- 	Bringing this up to date with the ActiveUser api.

--- trunk/modules/BentoBase/actions/LogIn.class.php
+++ trunk/modules/BentoBase/actions/LogIn.class.php
@@ -1,25 +1,25 @@
 <?php
 
-class BentoBaseActionLogIn extends PackageAction  
+class BentoBaseActionLogIn extends PackageAction
 {
 	static $requiredPermission = 'Read';
-	
+
 	public $AdminSettings = array('linkLabel' => 'Log In',
 									'linkTab' => 'Universal',
 									'headerTitle' => 'Log In',
 									'EnginePermissionOverride' => true);
-									
+
 	protected $form;
 	protected $loginSuccessful = false;
-	
+
 	protected function logic()
 	{
 		$info = InfoRegistry::getInstance();
-		
+
 		$form = new Form('logIn');
-		
+
 		$this->form = $form;
-		
+
 		$form->createInput('username')->
 				setLabel('Username: ')->
 			getForm()->
@@ -36,62 +36,62 @@ class BentoBaseActionLogIn extends PackageAction
 			try{
 				$inputHandler = $form->getInputhandler();
 				$active_user = ActiveUser::get_instance();
-				
-				if($active_user->change_user($inputHandler['username'], $inputHandler->get_raw('password')))
+
+				if($active_user->changeUser($inputHandler['username'], $inputHandler->getRaw('password')))
 				{
 					$this->loginSuccessful = true;
 				}
-			
+
 			}catch(Exception $e){
-				
+
 			}
-			
-		}	
+
+		}
 	}
-	
-	
+
+
 	public function viewHtml()
 	{
-		
+
 		if($this->loginSuccessful)
 		{
-			
+
 			$url = new Url();
 			//$url->fromString();
 			$post = Post::getInstance();
 
 			$info = InfoRegistry::getInstance();
-			
+
 			$url->engine =$info->Configuration['engine'];
-			
+
 			if(strlen($post['redirect']))
 			{
-			
+
 			}
-			
+
 		//	$this->engineHelper->page->addMeta('refresh', '5;url=' . $url);
 			return 'You have successfully logged in.';
-			
+
 		}else{
-			
+
 			if($this->form->wasSubmitted())
 			{
-				$this->AdminSettings['headerSubTitle'] = 'Invalid login';				
+				$this->AdminSettings['headerSubTitle'] = 'Invalid login';
 			}
-			
+
 			$output .= $this->form->makeDisplay();
-			
-		}	
-		
+
+		}
+
 		return $output;
 	}
-		
-	
+
+
 	public function viewAdmin()
 	{
 		return $this->viewHtml();
 	}
-									
+
 }
 
 
