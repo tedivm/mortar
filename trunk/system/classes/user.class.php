bbf6605dffcf05795b3c042ec7bccd672f1d518e
rhafner <rhafner@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2008-12-29 23:54:03 +0000

    Instantly destroying the old session when generating a new ID would occasionally result in lost sessions if the user tries to load a page before the browser had a chance to load the new session id (not an uncommon occurrence with ajax). This update keeps the old session alive for a limited amount of time before destroying it.

--- trunk/system/classes/user.class.php
+++ trunk/system/classes/user.class.php
@@ -249,6 +249,10 @@ class ActiveUser // extends User
 	private function __construct()
 	{
 		try{
+
+			if($_SESSION['OBSOLETE'] && ($_SESSION['EXPIRES'] < time()))
+				throw new BentoWarning('Attempt to use expired session.');
+
 			if(!is_numeric($_SESSION['user_id']))
 				throw new BentoNotice('No session started.');
 
@@ -261,9 +265,9 @@ class ActiveUser // extends User
 			if(!$this->loadUser($_SESSION['user_id']))
 				throw new BentoWarning('Attempted to log in user that does not exist with ID: ' . $_SESSION['user_id']);
 
-			if(mt_rand(1, 100) == 1)
+			if(!$_SESSION['OBSOLETE'] && mt_rand(1, 100) == 1)
 			{
-				session_regenerate_id(true);
+				$this->regenerateSession();
 			}
 
 		}catch(Exception $e){
@@ -293,7 +297,7 @@ class ActiveUser // extends User
 	protected function sessionStart($reload = false)
 	{
 		// Since the user is most likely changing, its a good time to switch the id up
-		session_regenerate_id(true);
+		$this->regenerateSession();
 
 		// This token is used by forms to prevent cross site forgery attempts
 		if(!isset($_SESSION['nonce']) || $reload)
@@ -428,6 +432,33 @@ class ActiveUser // extends User
 	public function getMemberGroups()
 	{
 		return $this->user->getMemberGroups();
+	}
+
+	protected function regenerateSession()
+	{
+		// Set current session to expire in 1 minute
+		$_SESSION['OBSOLETE'] = true;
+		$_SESSION['EXPIRES'] = time() + 60;
+
+		// Create new session without destroying the old one
+		session_regenerate_id(false);
+
+		// Grab current session ID and close both sessions to allow other scripts to use them
+		$newSession = session_id();
+		session_write_close();
+
+		// Set session ID to the new one, and start it back up again
+		session_id($newSession);
+		session_start();
+
+		// Don't want this one to expire
+		unset($_SESSION['OBSOLETE']);
+		unset($_SESSION['EXPIRES']);
+	}
+
+	protected function sessionCleanUp()
+	{
+
 	}
 
 }
