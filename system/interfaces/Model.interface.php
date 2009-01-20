06da33f1eff8f802f04fa9b229b60222bc31a362
Robert Hafner <tedivm@tedivm.com>
2009-01-20 07:52:47 +0000

    Better defined interface for models.

--- system/interfaces/Model.interface.php
+++ system/interfaces/Model.interface.php
@@ -2,9 +2,26 @@
 
 Interface ModelInterface
 {
-	public function __construct($locationId);
+	//this is required, but php fails in this respect
+//	static public $type;
 
-	public function save();
+	public function __construct($locationId = false);
+
+	public function name($name = NULL);
+
+	public function save($saveToLocation = false);
+
+	public function actionLookup($action);
+
+	public function createdDate($date = NULL);
+
+	public function getId();
+
+	// When the $user attribute isn't passed, you should use the active user
+	public function isAllowed($action, $user = NULL);
+
+	public function getParent();
+
 }
 
 
