221a706557466206782661122902926fbd3909af
Robert Hafner <tedivm@tedivm.com>
2009-01-20 07:51:12 +0000

    Updated actions to inherit from the "PackageAction" class.

--- modules/BentoBotch/actions/ResourceNotFound.class.php
+++ modules/BentoBotch/actions/ResourceNotFound.class.php
@@ -1,32 +1,32 @@
 <?php
 
-class BentoBotchActionResourceNotFound extends Action
+class BentoBotchActionResourceNotFound extends PackageAction
 {
-	
+
 	public $AdminSettings = array(	'linkTab' => 'Universal',
-									'headerTitle' => '404 Error');	
-	
+									'headerTitle' => '404 Error');
+
 	public function logic()
 	{
-		
+
 	}
-	
+
 	public function viewHtml()
 	{
 		$output = 'The page or resource you are looking for cannot be found.';
 		return $output;
 	}
-	
+
 	public function viewAdmin()
 	{
-		
+
 		$output = 'The page or resource you are looking for cannot be found.';
 		return $output;
 	}
-	
+
 	public function viewJson()
 	{
-		
+
 		return $output;
-	}	
+	}
 }
