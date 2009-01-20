86d26cbfda76293e6cbd75c958d9c5122c8c20bf
Robert Hafner <tedivm@tedivm.com>
2009-01-20 07:56:26 +0000

    Updated to reflect database changes.

--- system/classes/PackageList.class.php
+++ system/classes/PackageList.class.php
@@ -3,7 +3,7 @@
 class PackageList
 {
 	protected $packages = array();
-	
+
 	public function __construct($installedOnly = false)
 	{
 		if($installedOnly)
@@ -12,13 +12,13 @@ class PackageList
 		}else{
 			$this->loadPackages();
 		}
-		
+
 	}
-	
+
 	protected function loadPackages()
 	{
 		$info = InfoRegistry::getInstance();
-		
+
 		$packageDirectories = glob($info->Configuration['path']['modules'] . '*');
 		$packageList = array();
 		foreach ($packageDirectories as $packagePath)
@@ -26,34 +26,34 @@ class PackageList
 			$packageName = array_shift(explode('.', array_pop(explode('/', $packagePath))));
 			$packageList[$packageName] = new PackageInfo($packageName);
 		}
-		
+
 		$this->packages = $packageList;
 	}
-	
+
 	protected function loadInstalledPackages()
 	{
 		$db = dbConnect('default_read_only');
-		$results = $db->query('SELECT mod_package FROM modules');
+		$results = $db->query('SELECT package FROM modules');
 		$packageList = array();
 		while($row = $results->fetch_assoc())
 		{
-			$packages[] = $row['mod_package'];
-			$packageList[$row['mod_package']] = new PackageInfo($row['mod_package']);
+			$packages[] = $row['package'];
+			$packageList[$row['package']] = new PackageInfo($row['package']);
 		}
 
 		$this->packages = $packageList;
 	}
-	
+
 	public function getPackageList()
 	{
 		return array_keys($this->packages);
 	}
-	
+
 	public function getPackageDetails()
 	{
 		return $this->packages;
 	}
-	
+
 }
 
 ?>
