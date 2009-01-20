45a8220bb685ef9a01a69581f1526950f11fa31d
Robert Hafner <tedivm@tedivm.com>
2009-01-20 07:39:35 +0000

    Updates to the installation process to allow for the new model system.

--- modules/BentoBase/sql/system_install.sql.php
+++ modules/BentoBase/sql/system_install.sql.php
@@ -25,19 +25,21 @@ CREATE TABLE aliases
 	aliasOther VARCHAR(60) NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-/******************** Add Table: group_permissions ************************/
+/******************** Add Table: groupPermissions ************************/
 
 /* Build Table Structure */
-CREATE TABLE group_permissions
+CREATE TABLE groupPermissions
 (
 	memgroup_id INTEGER UNSIGNED NOT NULL,
-	perprofile_id INTEGER UNSIGNED NOT NULL,
-	location_id BIGINT UNSIGNED NOT NULL
+	location_id BIGINT UNSIGNED NOT NULL,
+	action_id INTEGER UNSIGNED NOT NULL,
+	permission VARCHAR(4) NOT NULL DEFAULT 'i',
+	resource VARCHAR(16) NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-/* Table Items: group_permissions */
-ALTER TABLE group_permissions ADD CONSTRAINT pkgroup_permissions
-	PRIMARY KEY (memgroup_id, location_id, perprofile_id);
+/* Table Items: groupPermissions */
+ALTER TABLE groupPermissions ADD CONSTRAINT pkgroupPermissions
+	PRIMARY KEY (memgroup_id, location_id, action_id, resource);
 
 /******************** Add Table: location_meta ************************/
 
@@ -60,9 +62,10 @@ CREATE TABLE locations
 (
 	location_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	location_parent BIGINT UNSIGNED NULL,
-	location_resource VARCHAR(16) NULL DEFAULT 'data',
+	location_resource VARCHAR(16) NOT NULL,
 	location_name VARCHAR(65) NULL,
 	inherit TINYINT UNSIGNED NULL DEFAULT 1,
+	defaultChild TINYINT UNSIGNED NULL,
 	location_createdOn DATETIME NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
@@ -70,7 +73,6 @@ CREATE TABLE locations
 
 /* Add Indexes for: locations */
 CREATE INDEX locations_location_parent_Idx ON locations (location_parent);
-CREATE UNIQUE INDEX locations_location_parent_location_name_Idx ON locations (location_parent, location_name);
 CREATE INDEX locations_location_resource_location_parent_Idx ON locations (location_resource, location_parent);
 
 /******************** Add Table: member_group ************************/
@@ -110,111 +112,32 @@ CREATE UNIQUE INDEX mod_config_mod_id_name_Idx ON mod_config (mod_id, name);
 CREATE TABLE modules
 (
 	mod_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	location_id BIGINT UNSIGNED NOT NULL,
-	mod_package VARCHAR(45) NOT NULL
+	package VARCHAR(45) NOT NULL,
+	lastupdated TIMESTAMP NULL,
+	majorVersion INTEGER UNSIGNED NULL DEFAULT 0,
+	minorVersion INTEGER UNSIGNED NULL DEFAULT 0,
+	microVersion INTEGER UNSIGNED NULL DEFAULT 0,
+	releaseType VARCHAR(12) NULL,
+	releaseVersion INTEGER UNSIGNED NULL,
+	status VARCHAR(12) NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 /* Table Items: modules */
 
-
-/******************** Add Table: package_installed ************************/
-
-/* Build Table Structure */
-CREATE TABLE package_installed
-(
-	name VARCHAR(60) NOT NULL,
-	lastupdated TIMESTAMP NULL,
-	majorVersion INTEGER UNSIGNED NOT NULL,
-	minorVersion INTEGER UNSIGNED NOT NULL DEFAULT 0,
-	microVersion INTEGER UNSIGNED NOT NULL DEFAULT 0,
-	prereleaseType VARCHAR(12) NULL,
-	prereleaseVersion INTEGER UNSIGNED NULL,
-	status VARCHAR(12) NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-/* Table Items: package_installed */
-ALTER TABLE package_installed ADD CONSTRAINT pkpackage_installed
-	PRIMARY KEY (name);
-
-/******************** Add Table: permission_profiles ************************/
+/******************** Add Table: modelsRegistered ************************/
 
 /* Build Table Structure */
-CREATE TABLE permission_profiles
+CREATE TABLE modelsRegistered
 (
-	perprofile_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	perprofile_name VARCHAR(25) NOT NULL,
-	perprofile_description TEXT NULL
+	name VARCHAR(20) NOT NULL PRIMARY KEY,
+	resource VARCHAR(16) NOT NULL,
+	mod_id INTEGER UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-/* Table Items: permission_profiles */
+/* Table Items: modelsRegistered */
 
-/* Add Indexes for: permission_profiles */
-CREATE UNIQUE INDEX permission_profiles_perprofile_name_Idx ON permission_profiles (perprofile_name);
-
-/******************** Add Table: permissionsprofile_has_actions ************************/
-
-/* Build Table Structure */
-CREATE TABLE permissionsprofile_has_actions
-(
-	action_id INTEGER UNSIGNED NOT NULL,
-	perprofile_id INTEGER UNSIGNED NOT NULL,
-	permission_status CHAR(1) NOT NULL DEFAULT 'n' COMMENT 'boolean'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-/* Table Items: permissionsprofile_has_actions */
-ALTER TABLE permissionsprofile_has_actions ADD CONSTRAINT pkpermissionsprofile_has_actions
-	PRIMARY KEY (action_id, perprofile_id);
-
-/******************** Add Table: plugin_lookup_engine ************************/
-
-/* Build Table Structure */
-CREATE TABLE plugin_lookup_engine
-(
-	mod_id INTEGER UNSIGNED NOT NULL,
-	plugin_engine VARCHAR(50) NOT NULL,
-	hook VARCHAR(72) NOT NULL,
-	name VARCHAR(40) NULL,
-	package_name VARCHAR(60) NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-/* Table Items: plugin_lookup_engine */
-
-/* Add Indexes for: plugin_lookup_engine */
-CREATE INDEX plugin_lookup_engine_plugin_engine_hook_Idx ON plugin_lookup_engine (plugin_engine, hook);
-
-/******************** Add Table: plugin_lookup_location ************************/
-
-/* Build Table Structure */
-CREATE TABLE plugin_lookup_location
-(
-	mod_id INTEGER UNSIGNED NOT NULL,
-	location_id BIGINT UNSIGNED NOT NULL,
-	hook VARCHAR(72) NOT NULL,
-	name VARCHAR(40) NULL,
-	package_name VARCHAR(60) NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-/* Table Items: plugin_lookup_location */
-
-/* Add Indexes for: plugin_lookup_location */
-CREATE INDEX plugin_lookup_location_location_id_hook_Idx ON plugin_lookup_location (location_id, hook);
-
-/******************** Add Table: plugin_lookup_module ************************/
-
-/* Build Table Structure */
-CREATE TABLE plugin_lookup_module
-(
-	mod_id INTEGER UNSIGNED NOT NULL,
-	module_package BIGINT NOT NULL,
-	hook VARCHAR(72) NOT NULL,
-	name VARCHAR(40) NOT NULL,
-	package_name VARCHAR(60) NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
-/* Table Items: plugin_lookup_module */
-
-/* Add Indexes for: plugin_lookup_module */
-CREATE INDEX plugin_lookup_module_hook_name_Idx ON plugin_lookup_module (hook, name);
+/* Add Indexes for: modelsRegistered */
+CREATE INDEX modelsRegistered_resource_Idx ON modelsRegistered (resource);
 
 /******************** Add Table: site_meta ************************/
 
@@ -265,19 +188,21 @@ CREATE TABLE user_in_member_group
 ALTER TABLE user_in_member_group ADD CONSTRAINT pkuser_in_member_group
 	PRIMARY KEY (user_id, memgroup_id);
 
-/******************** Add Table: user_permissions ************************/
+/******************** Add Table: userPermissions ************************/
 
 /* Build Table Structure */
-CREATE TABLE user_permissions
+CREATE TABLE userPermissions
 (
 	user_id INTEGER UNSIGNED NOT NULL,
-	perprofile_id INTEGER UNSIGNED NOT NULL,
-	location_id BIGINT UNSIGNED NOT NULL
+	location_id BIGINT UNSIGNED NOT NULL,
+	action_id INTEGER UNSIGNED NOT NULL,
+	permission VARCHAR(4) NOT NULL DEFAULT 'i',
+	resource VARCHAR(16) NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-/* Table Items: user_permissions */
-ALTER TABLE user_permissions ADD CONSTRAINT pkuser_permissions
-	PRIMARY KEY (user_id, location_id, perprofile_id);
+/* Table Items: userPermissions */
+ALTER TABLE userPermissions ADD CONSTRAINT pkuserPermissions
+	PRIMARY KEY (user_id, location_id, action_id, resource);
 
 /******************** Add Table: users ************************/
 
@@ -297,6 +222,8 @@ CREATE TABLE users
 CREATE INDEX user_name ON users (user_name, user_password);
 CREATE UNIQUE INDEX user_name_2 ON users (user_name);
 
+
+
 /************ Add Foreign Keys to Database ***************/
 
 /************ Foreign Key: fk_aliases_locations ***************/
@@ -311,17 +238,17 @@ ALTER TABLE aliases ADD CONSTRAINT fk_aliases_locations_result
 ALTER TABLE aliases ADD CONSTRAINT fk_aliases_locations_target
 	FOREIGN KEY (aliasLocation) REFERENCES locations (location_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-/************ Foreign Key: fk_group_permissions_locations ***************/
-ALTER TABLE group_permissions ADD CONSTRAINT fk_group_permissions_locations
+/************ Foreign Key: fk_groupPermissions_locations ***************/
+ALTER TABLE groupPermissions ADD CONSTRAINT fk_groupPermissions_locations
 	FOREIGN KEY (location_id) REFERENCES locations (location_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-/************ Foreign Key: fk_group_permissions_member_group ***************/
-ALTER TABLE group_permissions ADD CONSTRAINT fk_group_permissions_member_group
-	FOREIGN KEY (memgroup_id) REFERENCES member_group (memgroup_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
+/************ Foreign Key: fk_groupPermissions_actions ***************/
+ALTER TABLE groupPermissions ADD CONSTRAINT fk_groupPermissions_actions
+	FOREIGN KEY (action_id) REFERENCES actions (action_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-/************ Foreign Key: fk_group_permissions_permission_profiles ***************/
-ALTER TABLE group_permissions ADD CONSTRAINT fk_group_permissions_permission_profiles
-	FOREIGN KEY (perprofile_id) REFERENCES permission_profiles (perprofile_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
+/************ Foreign Key: fk_groupPermissions_member_group ***************/
+ALTER TABLE groupPermissions ADD CONSTRAINT fk_groupPermissions_member_group
+	FOREIGN KEY (memgroup_id) REFERENCES member_group (memgroup_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 /************ Foreign Key: fk_location_meta_locations ***************/
 ALTER TABLE location_meta ADD CONSTRAINT fk_location_meta_locations
@@ -335,45 +262,9 @@ ALTER TABLE locations ADD CONSTRAINT fk_locations_locations
 ALTER TABLE mod_config ADD CONSTRAINT fk_mod_config_modules
 	FOREIGN KEY (mod_id) REFERENCES modules (mod_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-/************ Foreign Key: fk_modules_locations ***************/
-ALTER TABLE modules ADD CONSTRAINT fk_modules_locations
-	FOREIGN KEY (location_id) REFERENCES locations (location_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-/************ Foreign Key: fk_permissionsprofile_has_actions_actions ***************/
-ALTER TABLE permissionsprofile_has_actions ADD CONSTRAINT fk_permissionsprofile_has_actions_actions
-	FOREIGN KEY (action_id) REFERENCES actions (action_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-/************ Foreign Key: fk_permissionsprofile_has_actions_permission_profiles ***************/
-ALTER TABLE permissionsprofile_has_actions ADD CONSTRAINT fk_permissionsprofile_has_actions_permission_profiles
-	FOREIGN KEY (perprofile_id) REFERENCES permission_profiles (perprofile_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-/************ Foreign Key: fk_plugin_lookup_engine_modules ***************/
-ALTER TABLE plugin_lookup_engine ADD CONSTRAINT fk_plugin_lookup_engine_modules
+/************ Foreign Key: fk_modelsRegistered_modules ***************/
+ALTER TABLE modelsRegistered ADD CONSTRAINT fk_modelsRegistered_modules
 	FOREIGN KEY (mod_id) REFERENCES modules (mod_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-/************ Foreign Key: fk_plugin_lookup_engine_package_installed ***************/
-ALTER TABLE plugin_lookup_engine ADD CONSTRAINT fk_plugin_lookup_engine_package_installed
-	FOREIGN KEY (package_name) REFERENCES package_installed (name) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-/************ Foreign Key: fk_plugin_lookup_location_locations ***************/
-ALTER TABLE plugin_lookup_location ADD CONSTRAINT fk_plugin_lookup_location_locations
-	FOREIGN KEY (location_id) REFERENCES locations (location_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-/************ Foreign Key: fk_plugin_lookup_location_modules ***************/
-ALTER TABLE plugin_lookup_location ADD CONSTRAINT fk_plugin_lookup_location_modules
-	FOREIGN KEY (mod_id) REFERENCES modules (mod_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-/************ Foreign Key: fk_plugin_lookup_location_package_installed ***************/
-ALTER TABLE plugin_lookup_location ADD CONSTRAINT fk_plugin_lookup_location_package_installed
-	FOREIGN KEY (package_name) REFERENCES package_installed (name) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-/************ Foreign Key: fk_plugin_lookup_module_modules ***************/
-ALTER TABLE plugin_lookup_module ADD CONSTRAINT fk_plugin_lookup_module_modules
-	FOREIGN KEY (mod_id) REFERENCES modules (mod_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-/************ Foreign Key: fk_plugin_lookup_module_package_installed ***************/
-ALTER TABLE plugin_lookup_module ADD CONSTRAINT fk_plugin_lookup_module_package_installed
-	FOREIGN KEY (package_name) REFERENCES package_installed (name) ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 /************ Foreign Key: fk_site_settings_sites ***************/
 ALTER TABLE site_meta ADD CONSTRAINT fk_site_settings_sites
@@ -395,14 +286,14 @@ ALTER TABLE user_in_member_group ADD CONSTRAINT fk_user_in_member_group_member_g
 ALTER TABLE user_in_member_group ADD CONSTRAINT fk_user_in_member_group_users
 	FOREIGN KEY (user_id) REFERENCES users (user_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-/************ Foreign Key: fk_user_permissions_locations ***************/
-ALTER TABLE user_permissions ADD CONSTRAINT fk_user_permissions_locations
+/************ Foreign Key: fk_userPermissions_users ***************/
+ALTER TABLE userPermissions ADD CONSTRAINT fk_userPermissions_users
+	FOREIGN KEY (user_id) REFERENCES users (user_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+/************ Foreign Key: fk_userPermissions_locations ***************/
+ALTER TABLE userPermissions ADD CONSTRAINT fk_userPermissions_locations
 	FOREIGN KEY (location_id) REFERENCES locations (location_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-/************ Foreign Key: fk_user_permissions_permission_profiles ***************/
-ALTER TABLE user_permissions ADD CONSTRAINT fk_user_permissions_permission_profiles
-	FOREIGN KEY (perprofile_id) REFERENCES permission_profiles (perprofile_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-/************ Foreign Key: fk_user_permissions_users ***************/
-ALTER TABLE user_permissions ADD CONSTRAINT fk_user_permissions_users
-	FOREIGN KEY (user_id) REFERENCES users (user_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
+/************ Foreign Key: fk_userPermissions_actions ***************/
+ALTER TABLE userPermissions ADD CONSTRAINT fk_userPermissions_actions
+	FOREIGN KEY (action_id) REFERENCES actions (action_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
