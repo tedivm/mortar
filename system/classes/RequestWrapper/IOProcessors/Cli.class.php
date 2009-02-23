03891bb35ea9f54bbc0554d081be434cdb4e899e
Robert Hafner <tedivm@tedivm.com>
2009-02-23 21:54:57 +0000

    RequestWrapper
    This class is essentially the dispatcher. It loads the user request, routes it, and handles any errors.
    
    This class has two types of helpers-
    
    IOProcessors do exactly that. Each processor handles inputs in a unique way- through the command line, over HTTP (relying on the GET variables for action routing), or REST (which uses HTTP but also relies a bit more heavily on header information and the other fun features of the HTTP protocol for figuring out whats going on). These classes also handle actually outputting the results the RequestWrapper  sends them.
    
    OutputControllers essentially handle all formating of data before it gets sent out. Currently we have output controllers for JSON, HTML and coming soon XML. A special controller is also set up for the web administration interface.
    
    This separation gives us flexibility when adding new ways to interact with the system, as a new format will be available across all methods of interaction and any new method of interacting with the system will, theoretically, be accessible by applications that have not necessarily been built with any consideration for that method.

---
+++ system/classes/RequestWrapper/IOProcessors/Cli.class.php
@@ -0,0 +1,75 @@
+<?php
+
+class IOProcessorCli
+{
+	public function __construct()
+	{
+		$this->initialize();
+	}
+
+	public function initialize()
+	{
+		$this->start();
+		$this->setEnvironment();
+	}
+
+	public function nextRequest()
+	{
+		return false;
+	}
+
+	public function output($output)
+	{
+		echo $output;
+	}
+
+	public function close()
+	{
+
+	}
+
+	protected function setEnvironment()
+	{
+		$query = Query::getQuery();
+		Form::disableXsfrProtection();
+		Form::$userInput = 'stdin';
+
+		if($query['disableCache'])
+			Cache::$runtimeDisable = true;
+
+		// poor windows people don't get this right now.
+		$isRoot = (function_exists('posix_getuid') && posix_getuid() === 0);
+
+
+
+
+		if(INSTALLMODE === true)
+			return;
+
+		$user = ActiveUser::getInstance();
+		switch (true) {
+			case ($query['username'] && $query['password']):
+				$user->changeUser($query['username'], $query['password']);
+				break;
+			case ($query['username'] && $isRoot):
+				$user->loadUserByName($query['username']);
+				break;
+
+			case ($isRoot):
+				$user->loadUser(1);
+				break;
+
+			default:
+				$user->loadUserByName('guest');
+				break;
+			}
+	}
+
+	protected function start()
+	{
+
+	}
+
+}
+
+?>
---
+++ system/classes/RequestWrapper/OutputControllers/Abstract.class.php
@@ -0,0 +1,139 @@
+<?php
+
+abstract class AbstractOutputController
+{
+	protected $action;
+	protected $format;
+	protected $permission;
+	protected $runMethod;
+
+	protected $resourceType;
+	protected $activeResource;
+
+	protected $contentFilters = array();
+	protected $outputFilters = array();
+
+	public function initialize($action)
+	{
+		if(!$this->checkAction($action))
+			throw new TypeMismatch(array('Action', $action));
+
+		$this->action = $action;
+
+		if(!$this->format)
+		{
+			$class = get_class($this);
+			$this->format = substr($class, 0, strpos($class, 'OutputController'));
+		}
+		$this->start();
+	}
+
+
+	public function getFinalOutput()
+	{
+		$actionResults = $this->processAction();
+		$this->bundleOutput($actionResults);
+
+		foreach($this->outputFilters as $filter)
+			$filter->update($this);
+
+		$output = $this->makeDisplayFromResource();
+		return $output;
+	}
+
+
+
+
+	public function getAction()
+	{
+		return $this->action;
+	}
+
+	public function getFormat()
+	{
+		return $this->format;
+	}
+
+	public function getResource()
+	{
+		return $this->activeResource;
+	}
+
+	public function addContentFilter($filterClass)
+	{
+		$this->contentFilters[] = $filterClass;
+	}
+
+	public function addOutputFilter($filterClass)
+	{
+		$this->outputFilters[] = $filterClass;
+	}
+
+
+	abstract protected function bundleOutput($output);
+
+	abstract protected function makeDisplayFromResource();
+
+	protected function start()
+	{
+
+	}
+
+
+
+	protected function processAction()
+	{
+		$runMethod = (isset($this->runMethod)) ? $this->runMethod : 'view' . $this->format;
+
+		$this->action->start();
+		$output = $this->action->$runMethod();
+		return $this->filterOutput($output);
+	}
+
+	protected function filterOutput($output)
+	{
+		foreach($this->contentFilters as $filter)
+			$output = $filter->update($this, $output);
+
+		return $output;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+	protected function checkAction($action)
+	{
+		if(!$action)
+			throw new TypeMismatch(array('Action', $action));
+
+		$query = Query::getQuery();
+		$settingsArrayName = $query['format'] . 'Settings';
+
+		$viewMethod = 'view' . $query['format'];
+
+		// check to see that it has an output for the current format
+		if(!in_array($viewMethod, get_class_methods($action)))
+			throw new ResourceNotFoundError('Action needs ' . $viewMethod . ' method to be available for this format');
+
+		if(isset($this->permission)
+			&& !isset($action->{$settingsArrayName}['EnginePermissionOverride']))
+		{
+			if(!$action->checkAuth(staticHack($formatFilter, 'permission')))
+				throw new AuthenticationError('Not allowed to access this engine at this location.');
+		}
+
+		return true;
+	}
+
+}
+
+
+?>
