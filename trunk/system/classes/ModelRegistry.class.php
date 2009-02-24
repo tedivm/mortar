255eddc5c554d3f09955ee993607f79c747c73ae
rhafner <rhafner@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2009-02-24 02:08:56 +0000

    Class converts a string into a model handler

---
+++ trunk/system/classes/ModelRegistry.class.php
@@ -0,0 +1,58 @@
+<?php
+
+class ModelRegistry
+{
+	protected static $handlerList;
+
+	static public function getHandler($name)
+	{
+		if(!is_array(self::$handlerList))
+			self::loadHandlers();
+
+		return (self::$handlerList[$name]) ? self::$handlerList[$name] : false;
+	}
+
+	static public function setHandler($resource, $module, $name = null)
+	{
+		$moduleInfo = new PackageInfo($module);
+
+		if(is_null($name))
+			$name = $resource;
+
+		$db = db_connect('default');
+		$insertStmt = $db->stmt_init();
+		$insertStmt->prepare('REPLACE INTO modelsRegistered (name, resource, mod_id) VALUES (?, ?, ?)');
+		$insertStmt->bind_param_and_execute('ssi', $name, $resource, $moduleInfo->getId());
+
+		Cache::clear('system', 'models', 'handlers');
+		self::loadHandlers();
+	}
+
+	static protected function loadHandlers()
+	{
+		$cache = new Cache('system', 'models', 'handlers');
+		$handlers = $cache->getData();
+
+		if(!$cache->cacheReturned)
+		{
+			$handlers = array();
+			$db = dbConnect('default_read_only');
+			$moduleRows = $db->query('SELECT * FROM modelsRegistered');
+
+			while($row = $moduleRows->fetch_assoc())
+			{
+				$moduleInfo = new PackageInfo($row['mod_id']);
+				$className = $moduleInfo->getName() . 'Model' . $row['name'];
+				$handlers[$row['resource']] = array('name' => $row['name'],
+													'module' => $row['mod_id'],
+													'resource' => $row['resource'],
+													'class' => $className);
+			}
+
+			$cache->storeData($handlers);
+		}
+		self::$handlerList = $handlers;
+	}
+}
+
+?>
