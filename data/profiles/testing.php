fba332b5578e7759b0f4ff5b10b87db70d83fe86
Robert Hafner <tedivm@tedivm.com>
2009-04-15 07:18:53 +0000

    Added a few 'profiles' to choose from when running BentoBase. These profiles define constants which affect how the system is run, thus allowing the user to pick the setup most suited for their needs.
    The profile is selected through the runtime.php file (I know the line is just a single include, so this file can easily be replaced by a symlink on most systems).
    
    Production (default)- profile shows no php or internal BentoBase errors, (such as exceptions that don't specifically trigger an error response from the system) and has caching enabled.
    
    Development- Debugging level 4, E_STRICT, depreciation warnings enabled, caching disabled.
    
    Testing- Debugging level 2, benchmark (kind of a misnomer, more like profiling) enabled.

---
+++ data/profiles/testing.php
@@ -0,0 +1,8 @@
+<?php
+define('DEBUG', 2);
+define('STRICT', false);
+define('IGNOREPERMISSIONS', false);	//FOR TESTING ONLY!!!!
+define('BENCHMARK', true);
+define('DISABLECACHE', false);
+define('DEPRECIATION_WARNINGS', false);
+?>
