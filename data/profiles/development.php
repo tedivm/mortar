f639ff9a72fdc18b4fd8135374ea6891b771c171
Robert Hafner <tedivm@tedivm.com>
2009-04-24 06:56:56 +0000

    Updated data profiles with new OUTPUT_COMPRESSION constants. Production doesn't output the compression informational headers, while development and testing do.

--- data/profiles/development.php
+++ data/profiles/development.php
@@ -5,4 +5,6 @@ define('IGNOREPERMISSIONS', false);	//FOR TESTING ONLY!!!!
 define('BENCHMARK', false);
 define('DISABLECACHE', true);
 define('DEPRECIATION_WARNINGS', true);
+define('OUTPUT_COMPRESSION', true);
+define('OUTPUT_COMPRESSION_HEADERS', true);
 ?>
