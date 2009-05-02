b1a5d06dd6040fa6d2ef1282c4a987f155b283c8
rhafner <rhafner@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2009-05-02 23:40:14 +0000

    Updated the ModuleInstaller to pull in custom code from the module. Created the CustomInstall plugin for the BentoBase class. This plugin registers plugins (currently just the AdminNav one) when the module is installed.
    Updated the AdminNav and Admin output controller to handle the new (more stripped down and semantic) navigation and theme information.

--- trunk/modules/BentoBase/plugins/AdminNav.class.php
+++ trunk/modules/BentoBase/plugins/AdminNav.class.php
@@ -2,42 +2,98 @@
 
 class BentoBasePluginAdminNav
 {
-	protected $links = array('Universal' => array());
-
+	protected $staticLinks = array('Universal' => array());
+	protected $dynamicLinks = array('Universal' => array());
 	public function __construct()
 	{
 		$url = new Url();
 		$url->module = 'BentoBase';
-		$url->action = 'LogIn';
+		$url->format = 'Admin';
+		$user = ActiveUser::getInstance();
 
-		$this->addLink('login', 'Log In', $url, 'Main', 'Pony');
-		$this->addLink('login', 'Log Out', $url, 'Main');
+		if($user->getName() == 'guest')
+		{
+			$url->action = 'LogIn';
+			$this->addDynamicLink('Log In', $url, 'Main', 'User');
+		}else{
+			$url->action = 'LogOut';
+			$this->addDynamicLink('Log Out', $url, 'Main', 'User');
+		}
+
+		$url = clone $url;
+		$url->action = 'Configuration';
+		$this->addStaticLink('Configuration', $url, 'System', 'Environment');
+
+		$url = clone $url;
+		$url->action = 'DatabaseSettings';
+		$this->addStaticLink('DatabaseSettings', $url, 'System', 'Environment');
+
+		$url = clone $url;
+		$url->action = 'ClearCache';
+		$this->addStaticLink('Clear Cache', $url, 'System', 'Environment');
+
+		$url = clone $url;
+		$url->action = 'InstallModule';
+		$this->addStaticLink('Install Module', $url, 'System', 'Modules');
+
+		$url = clone $url;
+		$url->action = 'AddUser';
+		$this->addStaticLink('Add User', $url, 'Users', 'Manage Users');
+
+		$url = clone $url;
+		$url->action = 'EditUser';
+		$this->addStaticLink('Edit User', $url, 'Users', 'Manage Users');
+
+		//$url->action = '';
+		//$this->addDynamicLink('name', 'label', $url, 'tab', 'category');
+
+
 	}
 
 	public function getTabs()
 	{
-		$keys = array_keys($this->links);
+		$keys = array_unique(array_merge(array_keys($this->staticLinks), array_keys($this->dynamicLinks)));
 		unset($keys['Universal']);
 		return $keys;
 	}
 
-	public function getNav($activeTab)
+	public function getStaticNav($activeTab)
 	{
-		$tabItems = (isset($this->links[$activeTab])) ? $this->links[$activeTab] : array();
-		return array_merge_recursive($tabItems, $this->links['Universal']);
+		$tabItems = (isset($this->staticLinks[$activeTab])) ? $this->staticLinks[$activeTab] : array();
+		return array_merge_recursive($tabItems, $this->staticLinks['Universal']);
 	}
 
+	public function getDynamicNav($activeTab)
+	{
+		$tabItems = (isset($this->dynamicLinks[$activeTab])) ? $this->dynamicLinks[$activeTab] : array();
+		return array_merge_recursive($tabItems, $this->dynamicLinks['Universal']);
+	}
 
-	protected function addLink($name, $label, Url $url, $tab, $category = 'StandAlone')
+	protected function addStaticLink($label, Url $url, $tab, $category = 'StandAlone', $name = null)
 	{
 		$action = array();
+
+		if(!isset($name))
+			$name = htmlentities(str_replace(' ', '_', $label));
+
 		$action['name'] = $name;
 		$action['label'] = $label;
 		$action['url'] = $url;
-		$this->links[$tab][$category][] = $action;
+		$this->staticLinks[$tab][$category][] = $action;
 	}
 
+	protected function addDynamicLink($label, Url $url, $tab, $category = 'StandAlone', $name = null)
+	{
+		$action = array();
 
+		if(!isset($name))
+			$name = htmlentities(str_replace(' ', '_', $label));
+
+		$action['name'] = $name;
+		$action['label'] = $label;
+		$action['url'] = $url;
+		$this->dynamicLinks[$tab][$category][] = $action;
+	}
 
 }
 
