1ecfb1c1efe8a6810cb5e62d5c86ad61e8074fb7
rhafner <rhafner@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2009-05-03 00:50:49 +0000

    Made it easier to change the hash method, and switched it to whirlpool (a strong, but somewhat time consuming, hash algorithm. For this case, slow is better).

--- trunk/system/classes/password.class.php
+++ trunk/system/classes/password.class.php
@@ -7,20 +7,26 @@ class Password
 	public $stored;
 	public $salt_start;
 	public $salt_end;
-	protected $salt_length = 15;
-	
+
+	// The stored result needs to fit in the database, which currently has a length of 192
+	// ((salt_length * 2) + 4 + algorithm hash length) <= 192
+	protected $salt_length = 30;
+	protected $cryptoAlgorithm = 'whirlpool';
+	// whirlpool length is 128
+	// whirlpool is really slow, which is good for a password hash as it makes generating rainbow tables more consuming
+
 	/*
-	
+
 	stored = salt::password::salt
-	
+
 	*/
-	
+
 }
 
 
 class StoredPassword extends Password
 {
-	
+
 	public function __construct($password)
 	{
 		$this->stored = $password;
@@ -30,16 +36,16 @@ class StoredPassword extends Password
 	public function is_match($password)
 	{
 		$test_password = new NewPassword($password, $this->salt_start, $this->salt_end);
-		
+
 		if($test_password->hash == $this->hash)
 		{
 			return true;
 		}else{
 			return false;
 		}
-		
+
 	}
-	
+
 	protected function get_salts()
 	{
 		$split = explode('::', $this->stored);
@@ -47,20 +53,19 @@ class StoredPassword extends Password
 		$this->salt_end = $split[1];
 		$this->hash = $split[2];
 	}
-	
-	
+
+
 }
 
 class NewPassword extends Password
 {
-	
+
 	public function __construct($password, $start = false, $end = false)
 	{
-		$this->salt_start = ($start) ? $start : substr(md5(time() * mt_rand()), 0, $this->salt_length);
-		$this->salt_end =  ($end) ? $end : substr(md5(time() * mt_rand()), 0, $this->salt_length);
-		
-		
-		$this->hash = hash('sha256', $this->salt_start . $password . $this->salt_end);
+		$this->salt_start = ($start) ? $start : substr(md5(uniqid(rand(), true)), 0, $this->salt_length);
+		$this->salt_end =  ($end) ? $end : substr(md5(uniqid(rand(), true)), 0, $this->salt_length);
+
+		$this->hash = hash($this->cryptoAlgorithm, $this->salt_start . $password . $this->salt_end);
 		$this->stored = $this->salt_start . '::' . $this->salt_end . '::' . $this->hash;
 	}
 
