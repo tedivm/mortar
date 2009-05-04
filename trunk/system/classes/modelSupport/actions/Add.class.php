2e38ab2005581e65fa02d6548e461b9c00c992cd
rhafner <rhafner@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2009-05-04 01:40:23 +0000

    Extended the Edit model class off of the Add one, and altered the index class to include them in the proper order.

--- trunk/system/classes/modelSupport/actions/Add.class.php
+++ trunk/system/classes/modelSupport/actions/Add.class.php
@@ -43,9 +43,7 @@ class ModelActionAdd extends FormActionBase
 		$query = Query::getQuery();
 		$formName = $this->type . 'Form';
 		$formClassName = importFromModule($formName, $this->model->getModule(), 'class', true);
-		//var_dump($formName);
-		//var_dump($this->model->getModule());
-		//var_dump($formClassName);
+
 		$form = new $formClassName($formName . $this->actionName);
 
 		$formExtension = $this->type . $query['format'] . 'Form';
@@ -57,14 +55,10 @@ class ModelActionAdd extends FormActionBase
 			$form->merge($formExtentionObject);
 		}
 
-
 		// If the parent location isn't set, there should be some form to do so.
 		if($this->model->getLocation()->getParent() === false)
 		{
 			throw new BentoError('Unspecified  parent', 400);
-//			$locationFormName = importClass('LocationForm', 'modelSupport/Forms/', 'class', true);
-//			$locationForm = new $locationFormName();
-//			$form->merge($locationForm);
 		}else{
 			$locationFormName = importClass('LocationForm', 'modelSupport/Forms/LocationForm.class.php',
 									 'mainclasses', true);
@@ -72,9 +66,6 @@ class ModelActionAdd extends FormActionBase
 			$form->merge($locationForm);
 			// parent is set
 		}
-
-
-
 		return $form;
 	}
 
@@ -82,24 +73,12 @@ class ModelActionAdd extends FormActionBase
 	protected function processInput($input)
 	{
 		$inputNames = array_keys($input);
-
-		foreach($inputNames as $name)
-		{
-			if(strpos($name, '_') !== false)
-			{
-				$nameValues = explode('_', $name);
-				if(isset($nameValues[1]))
-				{
-					$inputGroups[$nameValues[0]][] = $nameValues[1];
-				}
-			}
-		}
+		$inputGroups = $this->getInputGroups($inputNames);
 
 		foreach($inputGroups['model'] as $name)
 		{
 			$this->model[$name] = $input['model_' . $name];
 		}
-
 
 		$location = $this->model->getLocation();
 
@@ -118,6 +97,22 @@ class ModelActionAdd extends FormActionBase
 		return $this->model->save();
 	}
 
+
+	protected function getInputGroups($inputNames)
+	{
+		foreach($inputNames as $name)
+		{
+			if(strpos($name, '_') !== false)
+			{
+				$nameValues = explode('_', $name);
+				if(isset($nameValues[1]))
+				{
+					$inputGroups[$nameValues[0]][] = $nameValues[1];
+				}
+			}
+		}
+		return $inputGroups;
+	}
 
 	public function viewAdmin()
 	{
