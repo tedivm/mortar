bf2057606fbae9e2587a4784075540d70d1fe37a
Robert Hafner <tedivm@tedivm.com>
2009-05-07 01:52:14 +0000

    Added jsonp support to the json output formatter.

--- system/classes/RequestWrapper/OutputControllers/Json.class.php
+++ system/classes/RequestWrapper/OutputControllers/Json.class.php
@@ -11,7 +11,16 @@ class JsonOutputController extends AbstractOutputController
 
 	protected function makeDisplayFromResource()
 	{
-		return json_encode($this->activeResource);
+		$query = Query::getQuery();
+		$json = json_encode($this->activeResource);
+		if(isset($query['callback']))
+		{
+			$this->mimeType = 'application/javascript';
+			$callback = preg_replace('[^A-Za-z0-9]', '', $query['callback']);
+			return  $callback . '(' . $json . ')';
+		}else{
+			return $json;
+		}
 	}
 }
 
