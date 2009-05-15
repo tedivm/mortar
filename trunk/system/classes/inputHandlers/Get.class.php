9a5cdc3b79ce838c1a65d82ebd7244eebf59f9be
rhafner <rhafner@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2009-05-15 20:02:16 +0000

    Documentation update.

--- trunk/system/classes/inputHandlers/Get.class.php
+++ trunk/system/classes/inputHandlers/Get.class.php
@@ -1,21 +1,32 @@
 <?php
+/**
+ * BentoBase
+ *
+ * @copyright Copyright (c) 2009, Robert Hafner
+ * @license http://www.mozilla.org/MPL/
+ * @package System
+ * @subpackage RequestWrapper
+ */
 
+/**
+ * This class is used to retrieve Get inputs.
+ *
+ * @package System
+ * @subpackage UserInputs
+ */
 class Get
 {
-
+	/**
+	 * This function processes Get inputs and returns those inputs as an array. As part of the processing it strips out
+	 * any magic quotes, if they were added.
+	 *
+	 * @return array
+	 */
 	static public function getArray()
 	{
 		$queryArray = get_magic_quotes_gpc() ? array_map('stripslashes', $_GET) : $_GET;
 		return $queryArray;
 	}
-
-
-
-
-
-
-
-
 
 }
 
