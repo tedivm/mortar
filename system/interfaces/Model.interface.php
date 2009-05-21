9478fe52e8907f112a09b3f845e6369422777e12
Robert Hafner <tedivm@tedivm.com>
2009-05-21 20:39:57 +0000

    Documentation update.

--- system/interfaces/Model.interface.php
+++ system/interfaces/Model.interface.php
@@ -1,32 +1,188 @@
 <?php
+/**
+ * BentoBase
+ *
+ * @copyright Copyright (c) 2009, Robert Hafner
+ * @license http://www.mozilla.org/MPL/
+ * @package System
+ * @subpackage ModelSupport
+ */
 
+/**
+ * All models should follow this interface.
+ *
+ * @package System
+ * @subpackage ModelSupport
+ */
 Interface Model extends ArrayAccess
 {
-
+	/**
+	 * The id is passsed to the model to load it from storage.
+	 *
+	 * @param mixed $id
+	 */
 	public function __construct($id = null);
 
-	public function delete();
-	public function save();
+	/**
+	 * This function saves the model to the database. It takes an optional argument of a Location, which if passed
+	 * is where the model should be saved.
+	 *
+	 * @param null|Location $parent
+	 */
+	public function save($parent = null);
 
+	/**
+	 * This function should delete the stored verion of the mode.
+	 *
+	 * @return bool
+	 */
+	public function delete();
+
+	/**
+	 * This function takes in the action and checks to see if the user if allowed to perform it on the model. If the
+	 * user isn't passed, the current active user should be checked.
+	 *
+	 * @param string $action
+	 * @param User $user
+	 * @return bool
+	 */
 	public function checkAuth($action, $user = null);
 
+	/**
+	 * This function takes in a string, as an action type, and returns information about the action class used to
+	 * perform that action.
+	 *
+	 * @param string $actionName
+	 * @return array Defined indexes are 'classname' and 'path'
+	 */
 	public function getAction($actionName);
+
+	/**
+	 * Returns all of the content of a model, as an array.
+	 *
+	 * @return array
+	 */
 	public function getContent();
+
+	/**
+	 * Returns the ID for the model.
+	 *
+	 * @return int|mixed
+	 */
 	public function getId();
+
+	/**
+	 * Returns the location object the model is saved to.
+	 *
+	 * @return Location
+	 */
 	public function getLocation();
+
+	/**
+	 * This returns all the properties of the model as an array.
+	 *
+	 * @return array
+	 */
 	public function getProperties();
+
+	/**
+	 * This returns a string representing the type of model this is.
+	 *
+	 * @return string
+	 */
 	public function getType();
 
+	/**
+	 * This returns a string naming the module that this model is packaged with.
+	 *
+	 * @return string
+	 */
+	public function getModule();
+
+	/**
+	 * This class is used to set the current parent location.
+	 *
+	 * @param Location $parent
+	 */
 	public function setParent(Location $parent);
 
-	// class properties define content, the actual substance of the class
-	public function __get($name);
-	public function __set($name, $value);
-	public function __isset($name);
-	public function __unset($name);
+	/**
+	 * This function checks to see if the model can be saved to parent location of a certain resource type.
+	 *
+	 * @param string $resourceType
+	 * @return bool
+	 */
+	public function canSaveTo($resourceType);
 
+	/**
+	 * Returns a property of the model.
+	 *
+	 * @param string $offset
+	 * @return mixed
+	 */
+	public function __get($offset);
+
+	/**
+	 * Sets a property of the model.
+	 *
+	 * @param string|int $offset
+	 * @param string|mixed $value
+	 */
+	public function __set($offset, $value);
+
+	/**
+	 * Checks to see if a property is set.
+	 *
+	 * @param string|int $offset
+	 * @return bool
+	 */
+	public function __isset($offset);
+
+	/**
+	 * Unsets a property of the model.
+	 *
+	 * @param string|int $offset
+	 */
+	public function __unset($offset);
+
+	/**
+	 * Returns a content value of the model.
+	 *
+	 * @param string $name
+	 * @return mixed
+	 */
+	public function offsetGet($name);
+
+	/**
+	 * Sets a content value of the model.
+	 *
+	 * @param string|int $name
+	 * @param string|mixed $value
+	 */
+	public function offsetSet($name, $value);
+
+	/**
+	 * Checks to see if a content value is set.
+	 *
+	 * @param string|int $name
+	 * @return bool
+	 */
+	public function offsetExists($name);
+
+	/**
+	 * Unsets a content value of the model.
+	 *
+	 * @param string|int $name
+	 */
+	public function offsetUnset($name);
+
+	/**
+	 * Returns an array representation of the model.
+	 *
+	 * @return array
+	 */
 	public function __toArray();
-}
 
+}
 
 ?>
