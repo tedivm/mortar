6c8cb0c2e75568c445e4b33dd5f4486a7019b2b7
charlequin <charlequin@users.noreply.github.com>
2009-06-09 06:12:25 +0000

    Set DEPRECIATION_WARNINGS to false.

--- data/profiles/development.php
+++ data/profiles/development.php
@@ -4,8 +4,8 @@ define('ERROR_LOGGING', 3);
 define('STRICT', true);
 define('IGNOREPERMISSIONS', false);	//FOR TESTING ONLY!!!!
 define('BENCHMARK', false);
-define('DISABLECACHE', true);
-define('DEPRECIATION_WARNINGS', true);
+define('DISABLECACHE', false);
+define('DEPRECIATION_WARNINGS', false);
 define('OUTPUT_COMPRESSION', true);
 define('OUTPUT_COMPRESSION_HEADERS', true);
 define('CONCISE_HTML', false);
