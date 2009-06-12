fe627201eaeab762b07aedf8e9c287db8a99a429
Robert Hafner <tedivm@tedivm.com>
2009-06-12 19:43:51 +0000

    This is a rather big update to the Model system.
    Previously there was one type of model, that based on location. To allow us to manage all the date structures with the model system- for instance, editing Users- this update create new generic model classes and splits the location based models into their own subclass of those base models. This means not just changing the models but updating their actions as well.
    
    This change also touched the Query, Url and RequestWrapper classes in order to process non-location based models.

--- modules/BentoBase/classes/UserForm.class.php
+++ modules/BentoBase/classes/UserForm.class.php
@@ -8,27 +8,27 @@ class BentoBaseUserForm extends Form
 		$this->changeSection('Info');
 		$this->setLegend('Member Information');
 
-		$this->createInput('name')->
+		$this->createInput('model_name')->
 			setLabel('Display/Login Name')->
 			addRule('required');
 
-		$this->createInput('email')->
+		$this->createInput('model_email')->
 			setLabel('Email')->
 			addRule('email');
 
 
-		$this->createInput('login')->
+		$this->createInput('model_login')->
 			setType('checkbox')->
 			setLabel('Allow Login');
 
-		$this->createInput('password')->
+		$this->createInput('model_password')->
 			setType('password')->
 			setLabel('Password');
 
 		$this->createInput('password_verify')->
 			setType('password')->
 			setLabel('Verify Password')->
-			addRule('equalTo', '#' . $this->name . '_password');
+			addRule('equalTo', 'model_password');
 								// Set to the actual ID until I figure out a better way to handle that
 
 
@@ -44,7 +44,7 @@ class BentoBaseUserForm extends Form
 			if($memberGroup['memgroup_name'] == 'Guest')
 				continue;
 
-			$this->createInput('memberGroup')->
+			$this->createInput('model_memberGroup')->
 				setType('checkbox')->
 				setLabel($memberGroup['memgroup_name'])->
 				property('value', $memberGroup['memgroup_id']);
