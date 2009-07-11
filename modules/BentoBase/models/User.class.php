bd658541299b691fc55f3afa10dc756c7cfd9587
Robert Hafner <tedivm@tedivm.com>
2009-07-11 06:27:35 +0000

    Updated the User model to add documentation and a "loadByEmail" function (which, surprisingly enough, loads the user by an email address).

--- modules/BentoBase/models/User.class.php
+++ modules/BentoBase/models/User.class.php
@@ -5,6 +5,11 @@ class BentoBaseModelUser extends AbstractModel
 	static public $type = 'User';
 	protected $table = 'users';
 
+	/**
+	 * This function runs the parent function before updating the User's membergroups.
+	 *
+	 * @return bool
+	 */
 	public function save()
 	{
 		try
@@ -39,6 +44,14 @@ class BentoBaseModelUser extends AbstractModel
 		return true;
 	}
 
+	/**
+	 * This function runs the parent function to load the user information and then loads the user's membergroups into
+	 * an array.
+	 *
+	 * @cache models *type *id membegroups
+	 * @param int $id
+	 * @return bool
+	 */
 	protected function load($id)
 	{
 		if(parent::load($id))
@@ -77,6 +90,53 @@ class BentoBaseModelUser extends AbstractModel
 		return false;
 	}
 
+	/**
+	 * This function can be used to load a user from an email address. It looks up the user id based on the email
+	 * address and then runs the load function.
+	 *
+	 * @cache models *type loadByEmail *address
+	 * @param string $address
+	 * @return bool
+	 */
+	public function loadByEmail($address)
+	{
+		if(!filter_var($address, FILTER_VALIDATE_EMAIL))
+			throw new BentoError('You must pass an email address to the loadByEmail function.');
+
+		$cache = new Cache('models', $this->getType(), 'loadByEmail', $address);
+		$userId = $cache->getData();
+
+		if($cache->isStale())
+		{
+			$userLookup = new ObjectRelationshipMapper('users');
+			$userLookup->setColumnLimits(array('user_id'));
+			$userLookup->email = $address;
+
+			if($userLookup->select(1))
+			{
+				$userId = $userLookup->user_id;
+			}else{
+				$userId = false;
+			}
+			$cache->storeData($userId);
+		}
+
+		if(is_null($userId))
+		{
+			return $this->load($userId);
+		}else{
+			return false;
+		}
+
+	}
+
+	/**
+	 * This function intercepts calls to the parent class and function in order to encrypt the password before saving
+	 * it.
+	 *
+	 * @param string $name
+	 * @param mixed $value
+	 */
 	public function offsetSet($name, $value)
 	{
 		if($name == 'password')
