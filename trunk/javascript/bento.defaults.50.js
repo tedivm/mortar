3ed2b591c7151d1e3b39dec8cf2affe853cfc114
rhafner <rhafner@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2009-07-27 05:22:20 +0000

    Created a jquery plugin to act as a wrapper around the various form client side form actions. This wrapper adds the validation methods and iterates through all the form inputs, taking out data from the inputs using the meta data plugin to set up wysiwyg editors, autocomplete values, and various other things.

--- trunk/javascript/bento.defaults.50.js
+++ trunk/javascript/bento.defaults.50.js
@@ -1,15 +1,5 @@
 $.siteSetting.defaults.url = baseUrl + 'index.php?format=json&action=jsSettings&module=Mortar';
 
-if(typeof($.validator) == 'function')
-{
-	$.validator.setDefaults({
-		meta: "validation",
-
-		onsubmit: true
-
-	});
-}
-
 if(typeof($.fck) == 'object')
 {
 	$.fck.config.path = $.siteSetting('url').javascript + 'fckeditor/';
