c8cd49433d85f005c806cf0223b2463662980d2d
Robert Hafner <tedivm@tedivm.com>
2009-08-18 05:33:27 +0000

    Updated the development profile to disable output compression (which can have errors) and made the development profile the default in subversion.

--- data/profiles/development.php
+++ data/profiles/development.php
@@ -6,8 +6,8 @@ define('IGNOREPERMISSIONS', false);	//FOR TESTING ONLY!!!!
 define('BENCHMARK', false);
 define('DISABLECACHE', false);
 define('DEPRECIATION_WARNINGS', false);
-define('OUTPUT_COMPRESSION', true);
-define('OUTPUT_COMPRESSION_HEADERS', true);
+define('OUTPUT_COMPRESSION', false);
+define('OUTPUT_COMPRESSION_HEADERS', false);
 define('CONCISE_HTML', false);
 define('DISABLE_MINIFICATION', false);
 ?>
