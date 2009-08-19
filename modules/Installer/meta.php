3df2dfe58c7768d85333e1c1540f2f2667e3958d
Robert Hafner <tedivm@tedivm.com>
2009-08-19 05:33:20 +0000

    Updated version information for modules that have changed since the last time it was packaged.

--- modules/Installer/meta.php
+++ modules/Installer/meta.php
@@ -1,6 +1,6 @@
 <?php
 $packageName = 'Installer';
-$version = '0.1';
+$version = '0.2';
 $description = 'The installer does exactly that, installs the system. This should be deleted from the system after it
 is set up';
 
