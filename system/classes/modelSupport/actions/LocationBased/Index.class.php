8403bdfcb8950665b1dce54dafd35ec24aae2454
charlequin <charlequin@users.noreply.github.com>
2009-08-31 03:37:43 +0000

    Moved Index display functionality to a separate class.

--- system/classes/modelSupport/actions/LocationBased/Index.class.php
+++ system/classes/modelSupport/actions/LocationBased/Index.class.php
@@ -140,56 +140,9 @@ class ModelActionLocationBasedIndex extends ModelActionLocationBasedRead
 		$menu = $page->getMenu('actions', 'modelNav');
 		$this->makeModelActionMenu($menu, $this->model, 'Admin');
 
-		$theme = $page->getTheme();
-		$themeSettings = $theme->getSettings();
-
-		$table = new Table($this->model->getLocation()->getName() . '_listing');
-		$table->addClass('model-listing');
-		$table->addClass('index-listing');
-		$table->addClass($this->model->getLocation()->getName() . '-listing');
-		$table->enableIndex();
-
-		$table->addColumnLabel('model_type', 'Type');
-		$table->addColumnLabel('model_name', 'Name');
-		$table->addColumnLabel('model_title', 'Title');
-		$table->addColumnLabel('model_owner', 'Owner');
-		$table->addColumnLabel('model_creationTime', 'Created');
-		$table->addColumnLabel('model_lastModified', 'Last Modified');
-		$table->addColumnLabel('model_actions', 'Actions');
-
-		foreach($this->childModels as $model)
-		{
-			$modelActions = '';
-			$modelData = $model->getModelAs('Html');
-			$table->newRow();
-			$modelProperties = $modelData->getProperties();
-			$table->addField('model_type',
-					 isset($modelProperties['model_type']) ? $modelProperties['model_type'] : "");
-			$table->addField('model_name',
-					 isset($modelProperties['model_name']) ? "<a href='" . $modelProperties['permalink'] . "'>" . $modelProperties['model_name'] . "</a>" : "");
-			$table->addField('model_title',
-					 isset($modelProperties['model_title']) ? $modelProperties['model_title'] : "");
-			$table->addField('model_owner',
-					 isset($modelProperties['model_owner']) ? $modelProperties['model_owner'] : "");
-			$table->addField('model_creationTime',
-					 isset($modelProperties['model_creationTime']) ? date($this->indexDateFormat, $modelProperties['model_creationTime']) : "");
-			$table->addField('model_lastModified',
-					 isset($modelProperties['model_lastModified']) ? date($this->indexDateFormat, $modelProperties['model_lastModified']) : "");
-
-			foreach($modelProperties['model_action_list'] as $action) 
-			{
-				$actionDisplay = (isset($themeSettings['images']['action_images']) && $themeSettings['images']['action_images'] == true) ?
-						'<img alt="' . $action . '" src="' . $theme->getUrl() . $themeSettings['images'][$action . '_image'] . '" />' : $action; 
-				$modelActions .= '<li class="action action_' . $action . '"><a href="' . $modelProperties['model_action_' . $action] . '">' . $actionDisplay . '</a></li>'; 
-			}
-				
-			$table->addField('model_actions',
-					 isset($modelProperties['model_actions']) ? "<ul class='action_list'>" . /* $modelProperties['model_actions'] */ $modelActions . "</ul>" : "");
-			
-
-		}
-
-		return $table->makeHtml();
+		$indexList = new IndexDisplayList($this->model, $this->childModels, $page);
+		
+		return $indexList->getListing();
 	}
 
 
