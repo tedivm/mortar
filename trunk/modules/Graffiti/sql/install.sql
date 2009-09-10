f1859ec5e055ee169cb8e468b427529d8112a9eb
tedivm@tedivm.com <tedivm@tedivm.com@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2009-09-10 06:04:34 +0000

    Some updates to the Graffiti module- this module should still be considered uber-pre-alpha.

--- trunk/modules/Graffiti/sql/install.sql
+++ trunk/modules/Graffiti/sql/install.sql
@@ -21,58 +21,28 @@ CREATE TABLE graffitiLocationHasTags
 	tagId INTEGER UNSIGNED NOT NULL,
 	locationId INTEGER UNSIGNED NOT NULL,
 	userId INTEGER UNSIGNED NOT NULL,
-	createdOn TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB CHARACTER SET utf8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci DEFAULT COLLATE utf8_general_ci;
-
-/* Table Items: cmsContent */
-ALTER TABLE graffitiLocationHasTags ADD CONSTRAINT pkgraffitiLocationHasTags
-	PRIMARY KEY (locationId, tagId, userId);
-
-CREATE INDEX graffitiLocationHasTags_createdOn_Idx ON graffitiLocationHasTags (createdOn, tagId);
-
-
-/******************** Add Table: graffitiLocationHasWeightedTags ************************/
-
-/* Build Table Structure */
-CREATE TABLE graffitiLocationHasWeightedTags
-(
-	tagId INTEGER UNSIGNED NOT NULL,
-	locationId INTEGER UNSIGNED NOT NULL,
-	userId INTEGER UNSIGNED NOT NULL,
 	weight INTEGER UNSIGNED NOT NULL DEFAULT 1,
 	createdOn TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB CHARACTER SET utf8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci DEFAULT COLLATE utf8_general_ci;
 
 /* Table Items: cmsContent */
-ALTER TABLE graffitiLocationHasWeightedTags ADD CONSTRAINT pkgraffitiLocationHasWeightedTags
+ALTER TABLE graffitiLocationHasTags ADD CONSTRAINT pkgraffitiLocationHasWeightedTags
 	PRIMARY KEY (locationId, tagId, userId);
 
-CREATE INDEX graffitiLocationHasWeightedTags_weight_Idx ON graffitiLocationHasWeightedTags (locationId, tagId, weight);
-CREATE INDEX graffitiLocationHasWeightedTags_createdOn_Idx ON graffitiLocationHasWeightedTags (createdOn, tagId);
+CREATE INDEX graffitiLocationHasTags_weight_Idx ON graffitiLocationHasTags (locationId, tagId, weight);
+CREATE INDEX graffitiLocationHasTags_createdOn_Idx ON graffitiLocationHasTags (createdOn, tagId);
 
 
 /************ Add Foreign Keys to Database ***************/
 
-/************ Foreign Key: fk_graffitiLocationHasTags_locations ***************/
+/************ Foreign Key: fk_graffitiLocationHasWeightedTags_locations ***************/
 ALTER TABLE graffitiLocationHasTags ADD CONSTRAINT fk_graffitiLocationHasTags_locations
 	FOREIGN KEY (locationId) REFERENCES locations (location_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
 
-/************ Foreign Key: fk_graffitiLocationHasTags_graffitiTags ***************/
-ALTER TABLE graffitiLocationHasTags ADD CONSTRAINT fk_graffitiLocationHasTags_graffitiTags
-	FOREIGN KEY (tagId) REFERENCES graffitiTags (tagId) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-/************ Foreign Key: fk_graffitiLocationHasTags_users ***************/
-ALTER TABLE graffitiLocationHasTags ADD CONSTRAINT fk_graffitiLocationHasTags_users
-	FOREIGN KEY (userId) REFERENCES users (user_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-/************ Foreign Key: fk_graffitiLocationHasWeightedTags_locations ***************/
-ALTER TABLE graffitiLocationHasWeightedTags ADD CONSTRAINT fk_graffitiLocationHasWeightedTags_locations
-	FOREIGN KEY (locationId) REFERENCES locations (location_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
 /************ Foreign Key: fk_graffitiLocationHasWeightedTags_graffitiTags ***************/
-ALTER TABLE graffitiLocationHasWeightedTags ADD CONSTRAINT fk_graffitiLocationHasWeightedTags_graffitiTags
+ALTER TABLE graffitiLocationHasTags ADD CONSTRAINT fk_graffitiLocationHasWeightedTags_graffitiTags
 	FOREIGN KEY (tagId) REFERENCES graffitiTags (tagId) ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 /************ Foreign Key: fk_graffitiLocationHasWeightedTags_users ***************/
-ALTER TABLE graffitiLocationHasWeightedTags ADD CONSTRAINT fk_graffitiLocationHasWeightedTags_users
+ALTER TABLE graffitiLocationHasTags ADD CONSTRAINT fk_graffitiLocationHasTags_users
 	FOREIGN KEY (userId) REFERENCES users (user_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
