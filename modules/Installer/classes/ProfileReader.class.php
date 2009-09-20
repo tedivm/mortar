b694de61e4db2ea33334f8720870046224497ad6
Robert Hafner <tedivm@tedivm.com>
2009-09-20 23:45:27 +0000

    Updated the ProfileReader to deal with more information from the profile and to better handle errors.

--- modules/Installer/classes/ProfileReader.class.php
+++ modules/Installer/classes/ProfileReader.class.php
@@ -2,35 +2,120 @@
 
 class InstallerProfileReader
 {
+	static $path;
+
+	protected $aliases;
 	protected $modules;
 	protected $membergroups;
 	protected $users;
 	protected $locationTree;
 
-	public function loadProfile($xml)
+	public function loadProfile($name)
 	{
 		try
 		{
+			$xml = $this->getXmlFromFile($name);
 			$xml = new SimpleXMLElement($xml);
-			$this->modules = $this->getModuleInformation($xml->modules->module);
-			$this->membergroups = $this->getMemberGroupInformation($xml->membergroups);
-			$this->users = $this->getUserInformation($xml->users->user);
-			$this->locationTree = $this->getLocationInfo($xml->locations->location);
+			$xml = $xml->profile;
+
+			if(isset($xml->aliases))
+				$this->loadAliases($xml->aliases);
+
+			if(isset($xml->modules->module))
+				$this->loadModuleInformation($xml->modules->module);
+
+			if(isset($xml->membergroups))
+				$this->loadMemberGroupInformation($xml->membergroups);
+
+			if(isset($xml->users->user))
+				$this->loadUserInformation($xml->users->user);
+
+			if(isset($xml->locations->location))
+				$this->loadLocationInfo($xml->locations->location);
+
+			if(isset($xml['extends']))
+			{
+				$profile = new InstallerProfileReader();
+
+				if(!$profile->loadProfile($xml['extends']))
+					throw new CoreError('Unable to load parent profile ' . $xml['extends']);
+
+				if($parentAliases = $profile->getAliases())
+					$this->aliases = array_merge($parentAliases, $this->getAliases());
+
+				if($parentModules = $profile->getModules())
+					$this->modules = array_merge($parentModules, $this->getModules());
+
+				if($parentMembergroups = $profile->getMembergroups())
+					$this->membergroups = array_merge($parentMembergroups, $this->getMembergroups());
+
+				if($parentUsers = $profile->getUsers())
+					$this->users = array_merge($parentUsers, $this->getUsers());
+
+				if($parentLocationTree = $profile->getLocations())
+					$this->locationTree = array_merge($parentLocationTree, $this->getLocations());
+			}
+
 		}catch(Exception $e){
-			return false;
+			if($e instanceof CoreError)
+				throw $e;
+
+			throw new CoreError('Unable to load profile: ' . $e->getMessage());
 		}
+		var_dump($this);
 		return true;
 	}
 
-	protected function getModuleInformation(SimpleXMLElement $xml)
+	public function getAliases()
+	{
+		return (isset($this->aliases)) ? $this->aliases : array();
+	}
+
+	public function getModules()
+	{
+		return (isset($this->modules)) ? $this->modules : array();
+	}
+
+	public function getMembergroups()
+	{
+		return (isset($this->membergroups)) ? $this->membergroups : array();
+	}
+
+	public function getUsers()
+	{
+		return (isset($this->users)) ? $this->users : array();
+	}
+
+	public function getLocations()
+	{
+		return (isset($this->locationTree)) ? $this->locationTree : array();
+	}
+
+	protected function loadAliases(SimpleXMLElement $xml)
+	{
+		$aliases = array();
+		if(isset($xml->modelgroup))
+			foreach($xml->modelgroup as $modelGroup)
+			{
+				$name = (string) $modelGroup['name'];
+
+				if(isset($modelGroup->model))
+					foreach($modelGroup->model as $model)
+						$aliases['modelgroups'][$name][(string) $model] = (!isset($model['include']) || $model['include'] != 'false');
+			}
+
+			$this->aliases = $aliases;
+	}
+
+	protected function loadModuleInformation(SimpleXMLElement $xml)
 	{
 		foreach($xml as $module)
 			$modules[(string) $module['name']]['install'] = true;
 
-		return $modules;
+		$this->modules = $modules;
 	}
 
-	protected function getMemberGroupInformation(SimpleXMLElement $xml)
+	protected function loadMemberGroupInformation(SimpleXMLElement $xml)
 	{
 		$groups = array();
 		foreach($xml->system->group as $memberGroup)
@@ -39,31 +124,48 @@ class InstallerProfileReader
 		foreach($xml->user->group as $memberGroup)
 			$groups['user'][] = (string) $memberGroup;
 
-		return $groups;
+		$this->membergroups = $groups;
 	}
 
-	protected function getUserInformation(SimpleXMLElement $xml)
+	protected function loadUserInformation(SimpleXMLElement $xml)
 	{
 		$users = array();
 		foreach($xml as $user)
 		{
 			$name = (string) $user['name'];
 			$users[$name]['login'] = (isset($user['login']) && $user['login'] == 'true');
+			$users[$name]['form'] = (isset($user['form']) && $user['form'] == 'true');
 
 			foreach($user->group as $group)
 				$users[$name]['groups'][] = (string) $group;
 		}
-
-		return $users;
+		$this->users = $users;
 	}
 
-	protected function getLocationInfo(SimpleXMLElement $xml)
+	protected function loadLocationInfo(SimpleXMLElement $xml)
+	{
+		$locations = $this->getLocationInfoFromXml($xml);
+		$this->locationTree = $locations;
+	}
+
+	protected function getLocationInfoFromXml(SimpleXMLElement $xml, $parentName = null)
 	{
 		$locations = array();
+
+		if(isset($parentName))
+		{
+			$parentName .= '_';
+		}else{
+			$parentName = '';
+		}
+
 		foreach($xml as $location)
 		{
 			$locationName = (string) $location['name'];
 
+			$locations[$locationName]['longname'] = $parentName . $locationName;
+
+			$locations[$locationName]['inherits'] = (!isset($location['inherits']) || $location['inherits'] == 'true');
 
 			if(isset($location->option))
 			{
@@ -113,13 +215,25 @@ class InstallerProfileReader
 			if(isset($location->children))
 				foreach($location->children as $children)
 				{
-					$locations[$locationName]['children'][] = $this->getLocationInfo($children);
+					$locations[$locationName]['children'][] = $this->getLocationInfoFromXml($children,
+																			$locations[$locationName]['longname']);
 				}
 		}
-
 		return $locations;
 	}
 
+	protected function getXmlFromFile($fileName)
+	{
+		$path = self::$path . $fileName . '.xml';
+		if(file_exists($path) && is_readable($path))
+		{
+			$xml = file_get_contents($path);
+			return $xml;
+		}else{
+			throw new CoreError('No such installation profile at location ' . $path);
+		}
+		return false;
+	}
 }
 
 ?>
