8e63b73f424fbdd1a9f114105e90fd4ededcc7e4
tedivm@tedivm.com <tedivm@tedivm.com@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2009-09-26 08:14:34 +0000

    If that installer update doesn't warrant a version increment I sure as hell don't know what does.

--- trunk/modules/Installer/meta.php
+++ trunk/modules/Installer/meta.php
@@ -1,6 +1,6 @@
 <?php
 $packageName = 'Installer';
-$version = '0.3';
+$version = '0.4';
 $description = 'The installer does exactly that, installs the system. This should be deleted from the system after it
 is set up';
 $disableInstall = true;
