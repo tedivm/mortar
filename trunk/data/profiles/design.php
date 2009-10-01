80b1cf1977078a69636a65e135f71a62e22cb37c
tedivm@tedivm.com <tedivm@tedivm.com@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2009-10-01 07:43:27 +0000

    Altered the Development profile to better match how its actually used and added a new Design profile.

--- trunk/data/profiles/design.php
+++ trunk/data/profiles/design.php
@@ -1,13 +1,13 @@
 <?php
-define('DEBUG', 3);
+define('DEBUG', 0);
 define('ERROR_LOGGING', 3);
-define('STRICT', true);
+define('STRICT', false);
 define('IGNOREPERMISSIONS', false);	//FOR TESTING ONLY!!!!
 define('BENCHMARK', false);
 define('DISABLECACHE', true);
 define('DEPRECIATION_WARNINGS', false);
-define('OUTPUT_COMPRESSION', false);
-define('OUTPUT_COMPRESSION_HEADERS', false);
+define('OUTPUT_COMPRESSION', true);
+define('OUTPUT_COMPRESSION_HEADERS', true);
 define('CONCISE_HTML', false);
 define('DISABLE_MINIFICATION', true);
 ?>
