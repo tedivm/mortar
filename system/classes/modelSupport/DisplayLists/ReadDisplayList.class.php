b7258bc689f9fa81d864a0723ed5f4e87262a081
charlequin <charlequin@users.noreply.github.com>
2009-10-03 21:35:30 +0000

    Removed extraneous line in ReadDisplayList

--- system/classes/modelSupport/DisplayLists/ReadDisplayList.class.php
+++ system/classes/modelSupport/DisplayLists/ReadDisplayList.class.php
@@ -120,7 +120,6 @@ class ReadDisplayList implements DisplayList {
 			array_push($baseActionTypes, 'Index');
 
 		$actionUrls = $model->getActionUrls($format);
-		var_dump($actionUrls);
 		echo "<br /><br />";
 		$allowedUrls = array();
 
