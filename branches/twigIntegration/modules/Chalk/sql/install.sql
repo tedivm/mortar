5cb9a88a8d7933c7afa44b849877ba3d964a99a7
tedivm@tedivm.com <tedivm@tedivm.com@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2009-10-20 06:37:54 +0000

    This branch is no longer needed as it has been merged back into the trunk.

--- branches/twigIntegration/modules/Chalk/sql/install.sql
+++
@@ -1,9 +0,0 @@
-/******************** Add Table: Chalk_Blog ************************/
-
-/* Build Table Structure */
-CREATE TABLE Chalk_Blog
-(
-	id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	title TINYTEXT NULL,
-	subtitle TINYTEXT NULL
-) ENGINE=InnoDB CHARACTER SET utf8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci DEFAULT COLLATE utf8_general_ci;
