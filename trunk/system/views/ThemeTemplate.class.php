b75bb1eafeb9d1821b56347576a60c20541120c9
tedivm@tedivm.com <tedivm@tedivm.com@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2009-10-26 08:15:15 +0000

    Added better documentation to the ViewTemplateTwigLoader class.

--- trunk/system/views/ThemeTemplate.class.php
+++ trunk/system/views/ThemeTemplate.class.php
@@ -151,6 +151,9 @@ class ViewTemplateTwigLoader extends Twig_Loader_Filesystem
 		{
 			$fileContents = file_get_contents($this->lastLoadedList[$name]);
 
+			// We're iterating through the path list looking for the current class and then getting the name of the
+			// template after that, which is the parent template. We then takes the current template and replaces the
+			// call to parent with a call to the proper template name.
 			$found = false;
 			foreach($this->lastLoadedList as $className => $path)
 			{
