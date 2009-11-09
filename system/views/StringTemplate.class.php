dc315458196ca0d89333f75ab97a25c09f18ba69
Robert Hafner <tedivm@tedivm.com>
2009-11-09 00:06:35 +0000

    Added two new Views, both based off of the Templating system (Twig). One handles raw strings, the other makes it easier to handle loading model based templates.

---
+++ system/views/StringTemplate.class.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class ViewStringTemplate extends ViewThemeTemplate
+{
+	protected $twigLoader = 'Twig_Loader_String';
+
+	public function __construct($template)
+	{
+		$this->name = $template;
+	}
+
+	protected function getTwigLoader()
+	{
+		$config = Config::getInstance();
+
+		$cachePath = $config['path']['temp'] . '/twigCache/strings';
+		$loaderClass = $this->twigLoader;
+
+		$loader = new $loaderClass($cachePath);
+		return $loader;
+	}
+
+}
+
+?>
