11fd672d677059c24e9f67c257b2fdbae3c4c80b
Robert Hafner <tedivm@tedivm.com>
2009-11-29 08:31:45 +0000

    Made the MaintenanceMode message to users configurable.

--- modules/Rubble/actions/MaintenanceMode.class.php
+++ modules/Rubble/actions/MaintenanceMode.class.php
@@ -12,5 +12,15 @@ class RubbleActionMaintenanceMode extends RubbleActionAuthenticationError
 	protected $unauthenticatedErrorCode = 503;
 
 	protected $errorMessage = 'The system is currently down for maintenance.';
+
+	public function logic()
+	{
+		$config = Config::getInstance();
+		if(isset($config['system']['maintenanceMessage']))
+			$this->errorMessage = $config['system']['maintenanceMessage'];
+
+		return parent::logic();
+	}
+
 }
 ?>
