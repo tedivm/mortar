6dc063b72ef534e988068abfef7e49bbed788e7b
Robert Hafner <tedivm@tedivm.com>
2009-11-30 03:04:03 +0000

    Removed the abstract FormAction class from the ActionBase file.
    Updated the FormAction class.

--- system/abstracts/FormAction.class.php
+++ system/abstracts/FormAction.class.php
@@ -19,10 +19,54 @@ abstract class FormAction extends ActionBase
 	protected function getForm()
 	{
 		$formName = $this->formName;
-		return new $formName($this->actionName);
+		$form = new $formName(get_class($this));
+
+		return $form;
 	}
 
 	abstract protected function processInput($inputHandler);
+
+
+	public function viewAdminForm()
+	{
+		$output = '';
+		if($this->form->wasSubmitted())
+		{
+			if($this->formStatus)
+			{
+				$this->adminSuccess();
+			}else{
+				$this->adminError();
+			}
+		}else{
+
+		}
+
+		$query = Query::getQuery();
+		if(isset($query['message']) && method_exists($this, 'adminMessage'))
+		{
+			$this->adminMessage($query['message']);
+		}
+
+
+		$output .= $this->form->getFormAs('Html');
+		return $output;
+	}
+
+	protected function adminSuccess()
+	{
+
+	}
+
+	protected function adminError()
+	{
+
+	}
+
+	protected function adminMessage($messageId)
+	{
+
+	}
 }
 
 ?>
