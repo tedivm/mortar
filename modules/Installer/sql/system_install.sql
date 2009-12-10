36aa4e61fd7926131fb28b3b69318ed29ef1cdff
Robert Hafner <tedivm@tedivm.com>
2009-12-10 07:21:52 +0000

    Updated the database to add a new "schemaVersion" table for tracking the database (very meta).
    Added the structure for sql updates to the Mortar module, including the update script for adding the new schemaVersion table.

--- modules/Installer/sql/system_install.sql
+++ modules/Installer/sql/system_install.sql
@@ -259,6 +259,24 @@ CREATE INDEX requestLog_ioHandler_Idx ON requestLog (ioHandler);
 CREATE INDEX requestLog_format_Idx ON requestLog (format);
 CREATE INDEX requestLog_accessTime_Idx ON requestLog (accessTime);
 
+
+/******************** Add Table: schemaVersion ************************/
+
+/* Build Table Structure */
+CREATE TABLE schemaVersion
+(
+	package VARCHAR(65) NOT NULL PRIMARY KEY,
+	lastupdated TIMESTAMP NULL,
+	majorVersion INTEGER UNSIGNED NULL DEFAULT 0,
+	minorVersion INTEGER UNSIGNED NULL DEFAULT 0,
+	microVersion INTEGER UNSIGNED NULL DEFAULT 0
+) ENGINE=InnoDB CHARACTER SET utf8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci DEFAULT COLLATE utf8_general_ci;
+
+/* Table Items: modules */
+
+/* Add Indexes for: modules */
+CREATE INDEX majorVersion_minorVersion_microVersion_Idx ON schemaVersion (majorVersion, minorVersion, microVersion);
+
 /******************** Add Table: sites ************************/
 
 /* Build Table Structure */
@@ -451,3 +469,9 @@ ALTER TABLE userPermissions ADD CONSTRAINT fk_userPermissions_locations
 /************ Foreign Key: fk_userPermissions_actions ***************/
 ALTER TABLE userPermissions ADD CONSTRAINT fk_userPermissions_actions
 	FOREIGN KEY (action_id) REFERENCES actions (action_id) ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+
+
+/**** update version ****/
+REPLACE INTO schemaVersion (package, lastupdated, majorVersion, minorVersion, microVersion)
+						VALUES ( 'Core', NOW(), 0, 1, 1);
