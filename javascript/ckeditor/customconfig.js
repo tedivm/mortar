cb3a49116f0fc78ce5c01ab2e6ab85a96b09a1cd
charlequin <charlequin@users.noreply.github.com>
2009-12-21 02:14:19 +0000

    Another tweak to insure Font js and css are loaded from the correct location by ckeditor.

--- javascript/ckeditor/customconfig.js
+++ javascript/ckeditor/customconfig.js
@@ -6,6 +6,6 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 CKEDITOR.editorConfig = function( config )
 {
 	config.uiColor = '#2D5F8A';
-	config.contentsCss = 'module/Mortar/FontLookUp.css';
-	config.customConfig = 'module/Mortar/FontLookUp.js';
+	config.contentsCss = baseUrl + 'module/Mortar/FontLookUp.css';
+	config.customConfig = baseUrl + 'module/Mortar/FontLookUp.js';
 };
