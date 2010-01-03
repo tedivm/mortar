14dd64ab5b3441fa7a567eda46af00e57bc7f031
charlequin <charlequin@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2010-01-03 22:58:16 +0000

    Removing the menus branch, which has nobly served its purpose.

--- branches/menus/javascript/mortar.defaults.50.js
+++
@@ -1,7 +0,0 @@
-$.siteSetting.defaults.url = baseUrl + 'module/Mortar/jsSettings.json';
-
-if(typeof($.ckeditor) == 'object')
-{
-	$.ckeditor.config.path = $.siteSetting('url').javascript + 'ckeditor/';
-	window.CKEDITOR_BASEPATH = $.siteSetting('url').javascript + 'ckeditor/';
-}
