c574f7de8d6df656cee624366cf6eca1bc0ce340
Robert Hafner <tedivm@tedivm.com>
2010-01-22 19:44:38 +0000

    Updated Foundry's base SQL to use proper table namespacing and added a few new columns.

--- modules/Foundry/sql/install.sql
+++ modules/Foundry/sql/install.sql
@@ -1,46 +1,49 @@
-/******************** Add Table: repositories ************************/
+/******************** Add Table: foundryRepositories ************************/
 
 /* Build Table Structure */
-CREATE TABLE repositories
+CREATE TABLE foundryRepositories
 (
 	id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	priority TINYINT SIGNED NOT NULL DEFAULT 0,
 	activeForInstalls TINYINT NOT NULL DEFAULT 0,
 	published TINYINT NOT NULL DEFAULT 0,
-	lastupdate DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00'
+	lastupdated DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB CHARACTER SET utf8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci DEFAULT COLLATE utf8_general_ci;
 
 /* Add Indexes for: repositories */
-CREATE INDEX repositories_priority_activeForInstalls_id_Idx ON repositories (priority, activeForInstalls, id);
+CREATE INDEX foundryRepositories_priority_activeForInstalls_id_Idx
+		ON foundryRepositories (priority, activeForInstalls, id);
 
 
-/******************** Add Table: repositoriesInformation ************************/
+/******************** Add Table: foundryRepositoriesInformation ************************/
 
 /* Build Table Structure */
-CREATE TABLE repositoriesInformation
+CREATE TABLE foundryRepositoriesInformation
 (
 	id INTEGER UNSIGNED NOT NULL PRIMARY KEY,
+	name VARCHAR(65) NOT NULL,
 	website TEXT NOT NULL,
 	description TEXT NOT NULL,
+	serverSoftware VARCHAR(65),
 	url	TEXT NOT NULL
 ) ENGINE=InnoDB CHARACTER SET utf8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci DEFAULT COLLATE utf8_general_ci;
 
 
 
-/******************** Add Table: packages ************************/
+/******************** Add Table: foundryPackages ************************/
 
 /* Build Table Structure */
-CREATE TABLE packages
+CREATE TABLE foundryPackages
 (
 	id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(65) NOT NULL
 ) ENGINE=InnoDB CHARACTER SET utf8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci DEFAULT COLLATE utf8_general_ci;
 
 
-/******************** Add Table: packageInformation ************************/
+/******************** Add Table: foundryPackageInformation ************************/
 
 /* Build Table Structure */
-CREATE TABLE packageInformation
+CREATE TABLE foundryPackageInformation
 (
 	id INTEGER UNSIGNED NOT NULL PRIMARY KEY,
 
@@ -48,20 +51,20 @@ CREATE TABLE packageInformation
 
 
 
-/******************** Add Table: packageConflicts ************************/
+/******************** Add Table: foundryPackageConflicts ************************/
 
 /* Build Table Structure */
-CREATE TABLE packageConflicts
+CREATE TABLE foundryPackageConflicts
 (
 	id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(65) NOT NULL
 ) ENGINE=InnoDB CHARACTER SET utf8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci DEFAULT COLLATE utf8_general_ci;
 
 
-/******************** Add Table: packageDependencies ************************/
+/******************** Add Table: foundryPackageDependencies ************************/
 
 /* Build Table Structure */
-CREATE TABLE packageDependencies
+CREATE TABLE foundryPackageDependencies
 (
 	id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
 	package VARCHAR(65) NOT NULL,
@@ -73,18 +76,18 @@ CREATE TABLE packageDependencies
 
 
 /* Add Indexes for: repositories */
-CREATE INDEX repositories_priority_activeForInstalls_id_Idx ON repositories (id, type, package);
+CREATE INDEX foundryPackageDependencies_priority_activeForInstalls_id_Idx ON foundryPackageDependencies (id, type, package);
 
 
 
-/******************** Add Table: repoHasPackages ************************/
+/******************** Add Table: foundryRepoHasPackages ************************/
 
 /* Build Table Structure */
-CREATE TABLE repoHasPackages
+CREATE TABLE foundryRepoHasPackages
 (
 	repoId INTEGER UNSIGNED NOT NULL,
 	packageId INTEGER UNSIGNED NOT NULL,
 ) ENGINE=InnoDB CHARACTER SET utf8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci DEFAULT COLLATE utf8_general_ci;
 
-ALTER TABLE repoHasPackages ADD CONSTRAINT pkrepoHasPackages
+ALTER TABLE foundryRepoHasPackages ADD CONSTRAINT pkrepoHasPackages
 	PRIMARY KEY (repoId, packageId);
