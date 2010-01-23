7df42f4e24adb2cebf9d0e4b8404bafd0976fe59
Robert Hafner <tedivm@tedivm.com>
2010-01-23 10:04:37 +0000

    Updated the Backup action itself finish things up. Moved the directory creation to it instead of the backup class.

--- modules/Foundry/actions/Backup.class.php
+++ modules/Foundry/actions/Backup.class.php
@@ -2,12 +2,20 @@
 
 class FoundryActionBackup extends ActionSystem
 {
+
+	protected $errorMessage = 'Backup failed.';
+	protected $successMessage = 'System successfully backed up.';
+
 	protected function systemAction()
 	{
 		$config = Config::getInstance();
+		$backupPath = $config['path']['temp'] . 'backups/';
+
+		if(!is_dir($backupPath))
+			mkdir($backupPath, octdec('0755'));
 
 		$backup = new FoundryBackup();
-		$backup->setPath($config['paths']['temp'] . 'backups/');
+		$backup->setPath($backupPath);
 
 		return $backup->backup();
 	}
