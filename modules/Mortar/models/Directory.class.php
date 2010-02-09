d3263abfa470a8a83de4d4e86e13c737363330ae
charlequin <charlequin@users.noreply.github.com>
2010-02-09 04:39:14 +0000

    Removed the publishDate flag from the Directory model.

--- modules/Mortar/models/Directory.class.php
+++ modules/Mortar/models/Directory.class.php
@@ -3,7 +3,6 @@
 class MortarModelDirectory extends LocationModel
 {
 	static public $type = 'Directory';
-	static public $usePublishDate = true;
 	protected $table = 'directories';
 
 	public $allowedChildrenTypes = array('Directory');
