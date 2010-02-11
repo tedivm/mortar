062097bd63c5514a5baccd2ec1cc4aab67f74112
tedivm@tedivm.com <tedivm@tedivm.com@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2010-02-11 09:02:27 +0000

    Updated the Handler interface for Stash to have the various actions- get and store- take the key as arguments, making it so the handlers could be reused easier and removed the setup function requirement since it was no longer needed.
    Turned the clear and purge functions into non-static functions.

--- branches/cacherebuild/system/thirdparty/Stash/Handler.class.php
+++ branches/cacherebuild/system/thirdparty/Stash/Handler.class.php
@@ -10,22 +10,13 @@
 interface StashHandler
 {
 	/**
-	 * This function gets the key, as an array. It should save it and make sure that it is able to run. If this function
-	 * returns anything but true the cache will be disabled for that request.
-	 *
-	 * @param array $key
-	 * @return bool
-	 */
-	public function setup($key); // return boolean
-
-	/**
 	 * This function should return the data array, exactly as it was received by the storeData function, or false if it
 	 * is not present. This array should have a value for "createdOn" and for "return", which should be the data the
 	 * main script is trying to store.
 	 *
 	 * @return array
 	 */
-	public function getData();
+	public function getData($key);
 
 	/**
 	 * This function takes an array as its first argument and the expiration time as the second. This array contains two
@@ -37,7 +28,7 @@ interface StashHandler
 	 * @param int $expiration
 	 * @return bool
 	 */
-	public function storeData($data, $expiration);
+	public function storeData($key, $data, $expiration);
 
 	/**
 	 * This function should clear the cache tree using the key array provided. If called with no arguments the entire
@@ -46,9 +37,14 @@ interface StashHandler
 	 * @param null|array $key
 	 * @return bool
 	 */
-	static function clear($key = null);
+	public function clear($key = null);
 
-	static function purge();
+	/**
+	 * This function is used to remove expired items from the cache.
+	 *
+	 * @return bool
+	 */
+	public function purge();
 }
 
 ?>
