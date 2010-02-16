3181392da4bfdc0728d49e63124d4b613c6f217b
charlequin <charlequin@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2010-02-16 03:06:27 +0000

    dashboard branch: Added a general "list"-returning method to the ControlRegistry.

--- branches/dashboard/system/classes/ControlRegistry.class.php
+++ branches/dashboard/system/classes/ControlRegistry.class.php
@@ -18,7 +18,7 @@ class ControlRegistry
 		if(defined('INSTALLMODE') && INSTALLMODE == true)
 			return false;
 
-		$cache = CacheControl::getCache('controls', $format, $name);
+		$cache = CacheControl::getCache('controls', $format, 'single', $name);
 
 		$data = $cache->getData();
 		if($cache->isStale()) {
@@ -28,7 +28,7 @@ class ControlRegistry
 			$stmt->prepare('SELECT controlName, moduleId, controlClass
 					FROM plugins 
 					WHERE controlFormat = ? AND controlName = ?');
-			if($stmt->bindAndExecute('s', $format)) {
+			if($stmt->bindAndExecute('ss', $format, $name)) {
 				if($row = $stmt->fetch_array()) {
 					$className = importFromModule($row['controlClass'], $row['moduleId'], 'control'));
 					if($className !== false) {
@@ -39,7 +39,7 @@ class ControlRegistry
 			$cache->storeData($data);
 		}
 
-		if($data !== false) {
+		if(count($data) > 0) {
 			$class = importFromModule($data['controlClass'], $data['moduleId'], 'control');
 			try {
 				$control = new $class();
@@ -47,6 +47,39 @@ class ControlRegistry
 			return $control;
 		} else {
 			return false;
+		}
+	}
+
+	static public function listControls($format)
+	{
+		if(defined('INSTALLMODE') && INSTALLMODE == true)
+			return false;
+	
+		$cache = CacheControl::getCache('controls', $format, 'list');
+
+		$data = $cache->getData();
+		if($cache->isStale()) {
+			$data = array();
+			$db = DatabaseConnection::getConnection('default_read_only');
+			$stmt = $db->stmt_init();
+			$stmt->prepare('SELECT controlName, moduleId, controlClass
+					FROM plugins 
+					WHERE controlFormat = ?');
+			if($stmt->bindAndExecute('s', $format)) {
+				while($row = $stmt->fetch_array()) {
+					$className = importFromModule($row['controlClass'], $row['moduleId'], 'control'));
+					if($className !== false) {
+						$data[] = $row;
+					}
+				}
+			}
+			$cache->storeData($data);
+		}
+
+		if (count($data) === 0) {
+			return false;
+		} else {
+			return $data;
 		}
 	}
 }
