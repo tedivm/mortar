a1a5430f9ed571e50efc087b9259a931e2a0e246
charlequin <charlequin@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2010-02-21 06:39:28 +0000

    dashboard branch: Updated the ControlSet class to load User control configurations from the database.

--- branches/dashboard/system/classes/ControlSet.class.php
+++ branches/dashboard/system/classes/ControlSet.class.php
@@ -2,10 +2,57 @@
 
 class ControlSet
 {
-	protected $controls;
+	protected $controls = array();
+	protected $user;
 
+	public function __construct($user) {
+		$this->user = ModelRegistry::loadModel('User', $user);
+	}
 
+	public function loadControls()
+	{
+		if(defined('INSTALLMODE') && INSTALLMODE == true)
+			return false;
 
+		if($this->user === false)
+			return false;
+
+		$cache = CacheControl::getCache('controls', 'admin', 'user', $this->user->getId());
+
+		$data = $cache->getData();
+		if($cache->isStale()) {
+			$db = DatabaseConnection::getConnection('default_read_only');
+			$stmt = $db->stmt_init();
+
+			$stmt->prepare('SELECT instanceId, sequence, controlId, locationId
+					FROM dashboardControls
+					WHERE userId = ?');
+			$stmt->bindAndExecute('i', $this->user->getId());
+
+			while($row = $stmt->fetch_array()) {
+				$control = array('id' => $row['instanceId'], 'control' => $row['controlId'],
+						 'location' => $row['locationId']);
+
+				$set_stmt = $db->stmt_init();
+
+				$set_stmt->prepare('SELECT settingName, settingKey
+						    FROM dashboardControlSettings
+						    WHERE instanceId = ?');
+				$set_stmt->bindAndExecute('i', $row['instanceId']);
+
+				$settings = array();
+				while($set_row = $set_stmt->fetch_array()) {
+					$settings[$set_row['settingName']] = $set_row['settingKey'];
+				}
+				$control['settings'] = $settings;
+
+				$data[$row['sequence']] = $control;
+			}
+
+			$cache->storeData($data);
+		}
+		$this->controls = $data;
+	}
 }
 
 ?>
