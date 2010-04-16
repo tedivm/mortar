3ebf5bad07c23163c6cfadc0cae24ce49e7bbcc0
charlequin <charlequin@users.noreply.github.com>
2010-04-16 05:42:26 +0000

    Added initial support for Categories to the Mortar model.

---
+++ modules/Mortar/classes/CategoryForm.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class MortarCategoryForm extends Form
+{
+
+	protected function define()
+	{
+		$this->changeSection('Info');
+		$this->setLegend('Category Information');
+
+		$this->createInput('model_name')->
+			setLabel('Category Name')->
+			addRule('required');
+	}
+}
+
+?>
