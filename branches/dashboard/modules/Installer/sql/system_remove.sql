0b0c9d6f0550a83e6e2d447e8156ec4235f02f92
charlequin <charlequin@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2010-06-05 02:43:08 +0000

    Removed the out of date Dashboard branch.

--- branches/dashboard/modules/Installer/sql/system_remove.sql
+++
@@ -1,6 +0,0 @@
-SET foreign_key_checks = 0;
-DROP TABLE IF EXISTS `actions`, `aliases`, `controls`, `cronJobs`, `dashboardControls`, `dashboardControlSettings`,
-				`directories`, `errorLog`, `groupPermissions`, `locations`, `locationMeta`, 
-				`memberGroup`, `modelsRegistered`, `modules`,`modConfig`, `plugins`, `requestLog`,
-				`schemaVersion`, `sites`, `trash`, `urls`, `userPermissions`, `users`, 
-				`userInMemberGroup`, `lithoContent`, `lithoPages`;
