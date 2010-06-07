9208b12624f9925eccb0e7198e40a5519a2549ab
charlequin <charlequin@users.noreply.github.com>
2010-06-07 20:36:47 +0000

    Added a plugin to add the comment count (when relevant) to models.

--- modules/Tessera/install/post.php
+++ modules/Tessera/install/post.php
@@ -20,6 +20,7 @@ class TesseraInstallerPostscript
 		Hook::registerModelPlugin('All', 'getAllowedChildrenTypes', $this->packageId, 'AllowDiscussionType', true);
 		Hook::registerModelPlugin('All', 'firstSaveLocation', $this->packageId, 'AddDiscussionChild', true);
 		Hook::registerModelPlugin('All', 'extraContent', $this->packageId, 'ModelDisplayComments', true);
+		Hook::registerModelPlugin('All', 'toArray', $this->packageId, 'ModelCommentsToArray', true);
 	}
 }
 ?>
