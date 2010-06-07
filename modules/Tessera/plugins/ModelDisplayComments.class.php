c333f890eb2b0bb157e167545c968559107b602a
charlequin <charlequin@users.noreply.github.com>
2010-06-07 22:18:05 +0000

    Added a PostComment action to Tessera which can be viewed separately OR used as a destination for the default comment posting from the read action.

--- modules/Tessera/plugins/ModelDisplayComments.class.php
+++ modules/Tessera/plugins/ModelDisplayComments.class.php
@@ -2,19 +2,45 @@
 
 class TesseraPluginModelDisplayComments
 {
-
 	public function getExtraContent($model)
 	{
 		$type = $model->getType();
 
-		if(TesseraComments::canCommentModelType($type)) {
-			$loc = $model->getLocation();
-			if($commentLoc = $loc->getChildByName('discussion')) {
-				$discussion = $commentLoc->getResource();
-				$converter = $discussion->getModelAs('Html');
-				return array('comments' => $converter->getOutput());
-			}
-		}
+		$content = array();
+
+		if(!TesseraComments::canCommentModelType($type))
+			return $content;
+
+		$loc = $model->getLocation();
+
+		if(!($commentLoc = $loc->getChildByName('discussion')))
+			return $content;
+
+		$discussion = $commentLoc->getResource();
+		$converter = $discussion->getModelAs('Html');
+		$content['comments'] = $converter->getOutput();
+
+		if($commentLoc->getStatus() === 'Closed')
+			return $content;
+
+		$user = ActiveUser::getUser();
+		$name = $user['name'];
+
+		if($name === 'Guest')
+			$name = '';
+
+		$query = Query::getQuery();
+		$url = new Url();
+		$url->location = $query['location'];
+		$url->format = $query['format'];
+		$url->action = 'PostComment';
+
+		$form = new TesseraPostCommentForm('comment-form', $discussion, $name);
+		$form->setAction($url);
+
+		$content['commentform'] = $form->getFormAs('Html');
+
+		return $content;
 	}
 }
 
