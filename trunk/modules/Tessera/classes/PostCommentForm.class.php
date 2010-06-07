1d78bc883a36195efe0e066d6afb4d942753dd75
charlequin <charlequin@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2010-06-07 22:24:22 +0000

    Updated the PostComment form and action so that logged in users always post as themselves.

--- trunk/modules/Tessera/classes/PostCommentForm.class.php
+++ trunk/modules/Tessera/classes/PostCommentForm.class.php
@@ -18,14 +18,17 @@ class TesseraPostCommentForm extends Form
 			setLegend('Comment')->
 			setMarkup(Markup::loadModelEngine($this->discussion->getType()));
 
-		$this->createInput('comment_author')->
-			setLabel('Name')->
+		$input = $this->createInput('comment_author')->
+			setLabel('User Name')->
 			addRule('required')->
 			addRule('maxlength', 40)->
 			setValue($this->author);
 
+		if($this->author !== '')
+			$input->property('readonly', true);
+
 		$this->createInput('comment_text')->
-			setLabel('Text')->
+			setLabel('Comment Text')->
 			addRule('required')->
 			setType('richtext');
 	}
