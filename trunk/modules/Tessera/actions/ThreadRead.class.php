7a440352506da224cc0852d98c04162af00ae735
charlequin <charlequin@7dde9974-7f36-11de-93f9-a34e0ca7406f>
2010-06-08 05:45:08 +0000

    Updated Tessera to feature a "Quick Reply" for Threads that works on the same principle as the Comment action.

--- trunk/modules/Tessera/actions/ThreadRead.class.php
+++ trunk/modules/Tessera/actions/ThreadRead.class.php
@@ -3,6 +3,20 @@
 class TesseraActionThreadRead extends TesseraActionForumRead
 {
 	protected $listOptions = array('browseBy' => 'creationDate', 'order' => 'ASC');
+
+	public function viewHtml($page)
+	{
+		$query = Query::getQuery();
+		$url = new Url();
+		$url->location = $query['location'];
+		$url->format = $query['format'];
+		$url->action = 'PostReply';
+
+		$view = parent::viewHtml($page);
+		$form = new TesseraPostReplyForm('post-reply', $this->model);
+		$form->setAction($url);
+		return $view . $form->getFormAs('Html');
+	}
 }
 
 ?>
