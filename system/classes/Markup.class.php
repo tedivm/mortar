9c370821a192d5dece6bc7f0d9e7114c50f0263d
charlequin <charlequin@users.noreply.github.com>
2010-08-20 02:41:58 +0000

    Modified the Markup system to store locations, allowing Model types to change Markup only for specific locations.

--- system/classes/Markup.class.php
+++ system/classes/Markup.class.php
@@ -67,42 +67,62 @@ class Markup
 
 	static function loadModelEngine($resource, $setting = false)
 	{
+		if($resource instanceof Model) {
+			if(method_exists($resource, 'getLocation')) {
+				$location = $resource->getLocation();
+				if($parent = $location->getParent()) {
+					$id = $location->getId();
+					$parentM = $parent->getResource();
+				}
+			}
+
+			$resource = ModelRegistry::getIdFromType($resource->getType());
+		}
+
 		if(!is_numeric($resource))
 			$resource = ModelRegistry::getIdFromType($resource);
 
-		$cache = CacheControl::getCache('models', $resource, 'settings', 'markup');
+		if(!isset($id))
+			$id = 1;
+
+		$cache = CacheControl::getCache('models', $resource, 'settings', 'markup', $id);
 		$data = $cache->getData();
 
 		if($cache->isStale())
 		{
 			$stmt = DatabaseConnection::getStatement('default_read_only');
-			$stmt->prepare('SELECT markupEngine FROM markup WHERE modelId = ?');
-			$stmt->bindAndExecute('i', $resource);
+			$stmt->prepare('SELECT markupEngine FROM markup WHERE modelId = ? AND location = ?');
+			$stmt->bindAndExecute('ii', $resource, $id);
 
 			if($row = $stmt->fetch_array()) {
 				$data['value'] = ($row['markupEngine']);
 				$data['setting'] = true;
-			}else{
-				$data['setting'] = false;
-				$model = ModelRegistry::loadModel($resource);
-				$engine = staticHack(get_class($model), 'richtext');
-				if(isset($engine)) {
-					$data['value'] = $engine;
-				} else {
-					$data['value'] = self::$defaultEngine;
-				}
+			} else {
+				$data = false;
 			}
 			$cache->storeData($data);
 		}
 
 		if($setting) {
 			return ($data['setting'] ? $data['value'] : false);
-		} else {
+		}
+
+		if($data) {
 			return $data['value'];
+		} elseif(isset($parentM) && $result = self::loadModelEngine($parentM)) {
+			return $result;
+		} elseif($id === 1) {
+			$data['setting'] = false;
+			$model = ModelRegistry::loadModel($resource);
+			if($engine = staticHack(get_class($model), 'richtext')) {
+				return $engine;
+			} else {
+				return self::$defaultEngine;
+			}
 		}
 	}
 
-	static function setModelEngine($resource, $engine)
+	static function setModelEngine($resource, $engine, $loc = 1)
 	{
 		if(!is_numeric($resource))
 			$resource = ModelRegistry::getIdFromType($resource);
@@ -118,6 +138,7 @@ class Markup
 		$orm->modelId = $resource;
 		$orm->select();
 		$orm->markupEngine = $engine;
+		$orm->location = $loc;
 		$orm->save();
 	}
 
